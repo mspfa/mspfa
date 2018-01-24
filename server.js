6b0dc8acc69479a09ffa1a0a6f9ba5b18096a587
Miroware8 <grantgryczan@gmail.com>
2018-01-24 14:37:33 -0500

    Dynamic POST requests

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 console.log("< Server >");
 const ServeCube = require("servecube");
+const {html} = ServeCube;
 const youKnow = require("./data/youknow.js");
-let tls;
+const options = {
+	eval: v => {
+		return eval(v);
+	},
+	httpPort: 8082,
+	subdomain: ["", "d", "beta"],
+	errorRedirect: {
+		404: "/?s=20784"
+	}
+};
 try {
-	tls = {
+	options.tls = {
 		key: fs.readFileSync("certs/privkey1.pem"),
 		cert: fs.readFileSync("certs/cert1.pem"),
 		ca: fs.readFileSync("certs/chain1.pem")
 	};
 } catch(err) {}
-ServeCube.serve({
-	basePath: `${__dirname}/`,
-	httpPort: 8082,
-	tls,
-	subdomain: ["", "d", "beta"],
-	errorRedirect: {
-		404: "/?s=20784"
-	},
-	githubSecret: youKnow.github.secret
-});
+const cube = ServeCube.serve(options);
 const stdin = process.openStdin();
 stdin.on("data", function(input) {
 	console.log(eval(String(input)));
