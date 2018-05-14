77743862109797b7e16930a3c78836066f9fb677
Grant Gryczan <grantgryczan@gmail.com>
2018-05-14 16:53:34 -0400

    Cooldown on shilling and creativity

--- bikestuntsbot.js
+++ bikestuntsbot.js
@@ -61,51 +61,77 @@ client.on("message", async msg => {
 	if(msg.author.id !== "152282430915608578" && (lowerCaseContent.includes("grant") || lowerCaseContent.includes("miro") || lowerCaseContent.includes("cube"))) {
 		guild.members.get("152282430915608578").send(`${msg.author} has mentioned you in ${msg.channel}.`);
 	}
-	if(!isPublic || msg.content.includes("<@294635195439513601>")) {
+	if(!isPublic) {
 		respond(msg.channel);
-	} else if(prefix.test(content)) {
-		const member = msg.guild.member(msg.author) || await msg.guild.members.fetch(msg.author);
-		content = content.replace(prefix, "");
-		const contentSpaceIndex = content.indexOf(" ");
-		const contentLineIndex = content.indexOf("\n");
-		const index = Math.min((contentSpaceIndex !== -1) ? contentSpaceIndex : Infinity, (contentLineIndex !== -1) ? contentLineIndex : Infinity);
-		if(index !== Infinity) {
-			content = [content.slice(0, index), content.slice(index+1)];
-		} else {
-			content = [content];
+	} else {
+		if(msg.content.includes("<@294635195439513601>")) {
+			respond(msg.channel);
 		}
-		content[0] = content[0].toLowerCase();
-		if(content[0] === "help") {
-			msg.author.send("**There is no help for you now.**");
-		} else if(member.hasPermission(0x00002000)) {
-			if(content[0] === "react") {
-				if(msg.author.id === "152282430915608578") {
-					const emojis = content[1].split(" ");
-					msg.channel.messages.fetch({
-						limit: 1,
-						before: msg.id
-					}).then(msgs => {
-						const msg2 = msgs.first();
-						for(const v of emojis) {
-							if(v) {
-								msg2.react(v).catch(doNothing);
-							}
-						}
-						msg.delete();
-					});
+		if(msg.channel.id === "394162947867410434") {
+			if(msg.author.id !== data.lastShillAuthor) {
+				if(Date.now()-data.lastShillDate < 300000) {
+					msg.delete();
+					msg.author.send("You must wait at least five minutes after someone posts in <#394162947867410434> before posting there too.");
+				} else {
+					data.lastShillDate = Date.now();
+					data.lastShillAuthor = msg.author.id;
+					save();
 				}
-			} else if(content[0] === "delete") {
-				msg.delete().then(() => {
-					const messages = parseInt(content[1]);
-					if(!isNaN(content[1])) {
-						msg.channel.bulkDelete(parseInt(content[1])).catch(doNothing);
+			}
+		} else if(msg.channel.id === "394162913155219456" && (msg.attachments.size || msg.embeds.length) && msg.author.id !== data.lastCreativeAuthor) {
+			if(Date.now()-data.lastCreativeDate < 300000) {
+				msg.delete();
+				msg.author.send("You must wait at least five minutes after someone posts a creative work in <#394162913155219456> before posting yours too.");
+			} else {
+				data.lastCreativeDate = Date.now();
+				data.lastCreativeAuthor = msg.author.id;
+				save();
+			}
+		}
+		if(prefix.test(content)) {
+			const member = msg.guild.member(msg.author) || await msg.guild.members.fetch(msg.author);
+			content = content.replace(prefix, "");
+			const contentSpaceIndex = content.indexOf(" ");
+			const contentLineIndex = content.indexOf("\n");
+			const index = Math.min((contentSpaceIndex !== -1) ? contentSpaceIndex : Infinity, (contentLineIndex !== -1) ? contentLineIndex : Infinity);
+			if(index !== Infinity) {
+				content = [content.slice(0, index), content.slice(index+1)];
+			} else {
+				content = [content];
+			}
+			content[0] = content[0].toLowerCase();
+			if(content[0] === "help") {
+				msg.author.send("**There is no help for you now.**");
+			} else if(member.hasPermission(0x00002000)) {
+				if(content[0] === "react") {
+					if(msg.author.id === "152282430915608578") {
+						const emojis = content[1].split(" ");
+						msg.channel.messages.fetch({
+							limit: 1,
+							before: msg.id
+						}).then(msgs => {
+							const msg2 = msgs.first();
+							for(const v of emojis) {
+								if(v) {
+									msg2.react(v).catch(doNothing);
+								}
+							}
+							msg.delete();
+						});
 					}
-				});
-			} else if(content[0] === "eval") {
-				try {
-					eval(content[1].replace(evalTest, "$1"));
-				} catch(err) {
-					console.error(err);
+				} else if(content[0] === "delete") {
+					msg.delete().then(() => {
+						const messages = parseInt(content[1]);
+						if(!isNaN(content[1])) {
+							msg.channel.bulkDelete(parseInt(content[1])).catch(doNothing);
+						}
+					});
+				} else if(content[0] === "eval") {
+					try {
+						eval(content[1].replace(evalTest, "$1"));
+					} catch(err) {
+						console.error(err);
+					}
 				}
 			}
 		}
