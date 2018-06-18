eb0eff54dd36917cfb0c8ed5aa7b4cee451de504
J-block <nixshadow@nixshadow.com>
2018-06-18 15:56:15 -0700

    Update bikestuntsbot.js

--- bikestuntsbot.js
+++ bikestuntsbot.js
@@ -35,7 +35,7 @@ client.once("ready", () => {
 	client.user.setActivity("a song you like");
 });
 client.on("guildMemberAdd", member => {
-	general.send(`${member} Welcome to ${guild.name}! We don't get it either.`);
+	general.send(`${member} Welcome to ${guild.name}! Contrary to popular belief, this isn't actually hell.`);
 });
 client.on("typingStart", channel => {
 	if(channel.type === "dm") {
