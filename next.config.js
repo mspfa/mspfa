dbb945a9da709d84f71fbb0f7b5b49f57cdc4cd2
Grant Gryczan <grantgryczan@gmail.com>
2021-02-28 11:52:20 -0500

    Add custom Next config

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const { sep } = require('path');
+
+module.exports = {
+	webpack: config => {
+		// The following is to undo the default hashing of CSS module class names.
+		for (const { oneOf } of config.module.rules) {
+			if (Array.isArray(oneOf)) {
+				for (const rule of oneOf) {
+					if (!rule.sideEffects && Array.isArray(rule.use)) {
+						for (const entry of rule.use) {
+							if (entry.loader.includes(`${sep}css-loader${sep}`)) {
+								entry.options.modules.getLocalIdent = (context, localIdentName, localName) => localName;
+							}
+						}
+					}
+				}
+			}
+		}
+		return config;
+	}
+};
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,6 @@
-export default (req, res) => {
+import type express from 'express';
+
+export default (req: express.Request, res: express.Response) => {
 	res.statusCode = 200;
 	res.json({ name: 'John Doe' });
 };
