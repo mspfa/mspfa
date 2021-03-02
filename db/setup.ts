3cc0b7782923e7131809fab0c7045470694c58f2
Grant Gryczan <grantgryczan@gmail.com>
2021-03-02 05:18:12 -0500

    Rearrange type files

--- db/setup.ts
+++ db/setup.ts
@@ -1,4 +1,4 @@
-import type {} from '../types/global';
+import type {} from '../global';
 import dotenv from 'dotenv';
 dotenv.config({ path: `.env.${process.argv[2] || 'development'}` });
 import { db } from './connect';
--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
