1a376b9e82247d76c45ed7ed9a73175394575ae3
Grant Gryczan <grantgryczan@gmail.com>
2021-03-06 02:36:14 -0500

    Add lots of comments

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,11 +1,19 @@
 import NextLink from 'next/link';
 import type { AnchorHTMLAttributes } from 'react';
 
+// `href` is omitted here because NextLinkProps has a more inclusive `href`, allowing for URL objects in addition to strings.
 type AnchorProps = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>;
+// `passHref` is omitted here because it is not useful for this component. This component already passes the `href` value to the NextLink's child without `passHref`.
 type NextLinkProps = Omit<Parameters<typeof NextLink>[0], 'passHref'>;
 type LinkProps = AnchorProps & NextLinkProps;
 
-/** Accepts props from `a` elements and Next `Link` components, with the exception that `prefetch` on the latter defaults to `false`. */
+/**
+ * Should be used in place of `a`. Accepts any props which `a` accepts.
+ * 
+ * Also has all props from [Next's `Link` component](https://nextjs.org/docs/api-reference/next/link), except:
+ * - `prefetch` defaults to `false`.
+ * - `passHref` is removed because it is not useful for this component.
+ */
 const Link = (props: LinkProps) => {
 	const hrefString = String(props.href);
 	const external = /^(https?:)?\/\//i.test(hrefString);
@@ -17,15 +25,28 @@ const Link = (props: LinkProps) => {
 	delete anchorProps.children;
 	delete anchorProps.rel;
 	
+	let { rel } = props;
 	let nextLinkProps: NextLinkProps;
-	if (!external) {
-		// Set default NextLink props here.
+	if (external) {
+		// If an external link should open in a new tab, add `noreferrer noopener` to its `rel` for security reasons.
+		if (anchorProps.target === '_blank') {
+			if (rel) {
+				if (/(?:^| )(?:noreferrer|noopener)(?:$| )/i.test(rel)) {
+					throw new TypeError('If the `target` prop of a Link is `_blank`, its `rel` prop must not include `noreferrer` or `noopener` because they are included automatically.');
+				}
+				rel += ' noreferrer noopener';
+			} else {
+				rel = 'noreferrer noopener';
+			}
+		}
+	} else {
+		// Set default NextLink props (and `href`) here.
 		nextLinkProps = {
 			href: props.href,
 			prefetch: false
 		};
 		
-		// Attempt to set all applicable NextLink props (https://nextjs.org/docs/api-reference/next/link), and delete those props from the anchor.
+		// Attempt to set [all applicable NextLink props](https://nextjs.org/docs/api-reference/next/link), and delete those props from the anchor.
 		delete anchorProps.href;
 		if (anchorProps.as !== undefined) {
 			nextLinkProps.as = anchorProps.as;
@@ -53,19 +74,8 @@ const Link = (props: LinkProps) => {
 		}
 	}
 	
-	let relationship = props.rel;
-	if (external && anchorProps.target === '_blank') {
-		if (relationship) {
-			if (/(?:^| )(?:noreferrer|noopener)(?:$| )/i.test(relationship)) {
-				throw new TypeError('If the `target` prop of a `Link` is `_blank`, its `rel` prop must not include `noreferrer` or `noopener` because they are included automatically.');
-			}
-			relationship += ' noreferrer noopener';
-		} else {
-			relationship = 'noreferrer noopener';
-		}
-	}
 	const anchor = (
-		<a {...anchorProps} rel={relationship}>
+		<a {...anchorProps} rel={rel}>
 			{props.children}
 		</a>
 	);
