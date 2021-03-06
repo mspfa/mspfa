a8187b048f220cf7a33ecdaf836e500d4610e913
Grant Gryczan <grantgryczan@gmail.com>
2021-03-06 01:42:18 -0500

    Filter out non-images in the footer

--- pages/api/footer.ts
+++ pages/api/footer.ts
@@ -5,7 +5,7 @@ import fs from 'fs-extra';
 const footerDir = path.join(process.cwd(), '/public/images/footers');
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const footers = await fs.readdir(footerDir);
+	const footers = (await fs.readdir(footerDir)).filter(footer => /\.(?:png|gif)$/i.test(footer));
 	res.status(200).send({
 		name: footers[Math.floor(Math.random() * footers.length)]
 	});
