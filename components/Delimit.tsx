06ecd37739893e1e26d166896172fd6b591c7e87
Grant Gryczan <grantgryczan@gmail.com>
2021-03-08 01:13:16 -0500

    Accept elements in Delimit and not element types

--- components/Delimit.tsx
+++ components/Delimit.tsx
@@ -1,53 +1,55 @@
-import { Fragment } from 'react';
+import React from 'react';
 
 /**
+ * ⚠️ To increase performance and simplicity, please avoid using this.
+ *
  * Inserts the delimiter element set in the `with` prop between each child of this component.
- * 
- * This component's props are spread to each inserted delimiter.
  * 
  * Each child has a `key` prop which defaults to its index in this component's children.
  * 
  * Example:
  * ```
- * <Delimit with={Stick} className="my-delimiter">
- *     <span>example 1</span>
- *     <span>example 2</span>
- *     <span key="my-key">example 3</span>
+ * <Delimit with={<span className="delimiter"> | </span>}>
+ * 	<span>example 1</span>
+ * 	<span>example 2</span>
+ * 	<span key="my-key">example 3</span>
  * </Delimit>
  * ```
  * 
- * Approximate output:
+ * Example output:
  * ```
- * <Fragment key={0}>
- *     <span>example 1</span>
- *     <Stick className="my-delimiter" />
- * </Fragment>
- * <Fragment key={1}>
- *     <span>example 2</span>
- *     <Stick className="my-delimiter" />
- * </Fragment>
- * <Fragment key="my-key">
- *     <span>example 3</span>
- * </Fragment>
+ * <>
+ * 	{[
+ * 		<React.Fragment key={0}>
+ * 			<span>example 1</span>
+ * 		</React.Fragment>,
+ * 		<React.Fragment key={1}>
+ * 			<span className="delimiter"> | </span>
+ * 			<span>example 2</span>
+ * 		</React.Fragment>,
+ * 		<React.Fragment key="my-key">
+ * 			<span className="delimiter"> | </span>
+ * 			<span>example 3</span>
+ * 		</React.Fragment>
+ * 	]}
+ * </>
  * ```
  */
-const Delimit = <DelimiterType extends (props: any) => JSX.Element>(
-	{ children, with: Delimiter, ...props }: {
-		children: JSX.Element | JSX.Element[],
-		/** The element to delimit this component's children with. */
-		with: DelimiterType
-	} & Omit<Parameters<DelimiterType>[0], 'children' | 'with'>
-) =>
+const Delimit = ({ children = [], with: delimiter }: {
+	children?: JSX.Element | JSX.Element[],
+	/** The element to delimit this component's children with. */
+	with: JSX.Element
+}) =>
 	Array.isArray(children)
 		? (
 			<>
 				{children.map((child, index) => (
-					<Fragment
+					<React.Fragment
 						key={child.key === null ? index : child.key}
 					>
+						{index !== 0 && delimiter}
 						{child}
-						{index !== children.length - 1 && <Delimiter {...props as any} />}
-					</Fragment>
+					</React.Fragment>
 				))}
 			</>
 		)
--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -3,8 +3,8 @@ import Stick from '../Stick';
 
 const Nav = () => (
 	<nav>
-		<Delimit with={Stick}>
-			<span className="test">link 1</span>
+		<Delimit with={<Stick />}>
+			<span>link 1</span>
 			<span>link 2</span>
 			<span>link 3</span>
 		</Delimit>
