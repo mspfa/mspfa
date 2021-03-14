f5acaf6052cb1ad6a66fb51aae6d5f1ee2e188a3
Grant Gryczan <grantgryczan@gmail.com>
2021-03-14 01:12:42 -0500

    Improve mobile styles

--- components/FlashyTitle/index.tsx
+++ components/FlashyTitle/index.tsx
@@ -4,14 +4,14 @@ import './styles.module.scss';
 const NUMBER_OF_FLASHY_TITLES = 6;
 
 const FlashyTitle = () => (
-	<>
+	<div id="flashy-title-container">
 		<style jsx global>{`
 			#flashy-title {
 				background-image: url(/images/flashy-titles/${1 + Math.floor(Math.random() * NUMBER_OF_FLASHY_TITLES)}.png);
 			}
 		`}</style>
 		<Link id="flashy-title" href="/" title="MSPFA Home" tabIndex={-1} draggable={false} />
-	</>
+	</div>
 );
 
 export default FlashyTitle;
