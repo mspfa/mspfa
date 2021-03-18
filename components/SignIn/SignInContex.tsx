67359e2cfe5a37fa71f2ca005fea4fc02b2c3f3f
GrantGryczan <grantgryczan@gmail.com>
2021-03-18 19:36:35 -0400

    Sign-in buttons

--- components/SignIn/SignInContex.tsx
+++ components/SignIn/SignInContex.tsx
@@ -1,7 +1,7 @@
 import './styles.module.scss';
 
 const SignInContent = () => (
-	<>
+	<div id="sign-in-content">
 		<div id="sign-in-method-password">
 			<label htmlFor="email">
 				Email:
@@ -12,7 +12,12 @@ const SignInContent = () => (
 			</label>
 			<input id="password" name="password" type="password" required autoComplete="current-password" />
 		</div>
-	</>
+		<p>or</p>
+		<div id="sign-in-method-other">
+			<button id="sign-in-method-google" type="button">Google</button>
+			<button id="sign-in-method-discord" type="button">Discord</button>
+		</div>
+	</div>
 );
 
 export default SignInContent;
