82efcf286fcc8e6b86157f800e902978624d28ee
GrantGryczan <grantgryczan@gmail.com>
2021-03-20 05:10:07 -0400

    Respond to API with error 405 when appropriate

--- pages/api/sessions/index.ts
+++ pages/api/sessions/index.ts
@@ -3,5 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method === 'POST') {
 		
+	} else {
+		res.status(405).end();
 	}
 };
