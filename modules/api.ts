0a4def432ba57b5f22430b42c09611dbe8ef8a3c
GrantGryczan <grantgryczan@gmail.com>
2021-03-20 04:51:22 -0400

    Cleaner request code using interceptors

--- modules/api.ts
+++ modules/api.ts
@@ -1,28 +1,27 @@
 import axios from 'axios';
-import type { AxiosResponse } from 'axios';
 import { Dialog } from './dialogs';
 
-const instance = axios.create();
-
-export const methods = ['get', 'delete', 'head', 'options', 'post', 'put', 'patch'] as const;
-export type Method = typeof methods extends ReadonlyArray<infer T> ? T : never;
-
-type RequestArgs = Parameters<typeof instance.get>;
-
-const request = <T = any, R = AxiosResponse<T>>(
-	method: Method,
-	...args: RequestArgs
-) => instance[method]<T, R>(...args).catch(error => {
+/** This function works as if it is automatically plugged into every API call's `.catch`. */
+const handleReject = (error: any) => {
+	console.error(error);
+	
 	new Dialog({
 		title: 'Error',
 		content: String(error.message)
 	});
+	
 	return Promise.reject(error);
-});
+};
 
-const api: Record<Method, typeof instance.get> = {} as any;
-for (const method of methods) {
-	api[method] = (...args: RequestArgs) => request(method, ...args);
-}
+const api = axios.create();
+
+api.interceptors.request.use(
+	undefined,
+	handleReject
+);
+api.interceptors.response.use(
+	undefined,
+	handleReject
+);
 
 export default api;
