935ebc0839839c8ea9fd7b2a27cc391b2ffc8698
GrantGryczan <grantgryczan@gmail.com>
2021-03-21 23:02:17 -0400

    Do a better job deleting schemas, hide them in VSC

--- dev/schemas.ts
+++ dev/schemas.ts
@@ -13,31 +13,36 @@ const updated: Record<string, number> = {};
 
 fs.watch('pages/api', { recursive: true }, async (evt, filename) => {
 	const path = `pages/api/${filename}`;
-	if (path.endsWith('.ts') && !path.endsWith('.validate.ts') && (!(path in updated) || Date.now() - updated[path] > 1000)) {
-		updated[path] = Date.now();
+	if (path.endsWith('.ts') && !path.endsWith('.validate.ts')) {
 		const outputPath = `${path.slice(0, -3)}.validate.ts`;
 		const outputExists = fs.existsSync(outputPath);
 		if (fs.existsSync(path)) {
-			try {
-				const schemaString = JSON.stringify(
-					createGenerator({
-						path,
-						tsconfig: 'tsconfig.json'
-					}).createSchema('Request'),
-					null,
-					'\t'
-				);
-				if (!outputExists) {
-					await fs.createFile(outputPath);
+			if (!(path in updated) || Date.now() - updated[path] > 1000) {
+				updated[path] = Date.now();
+				try {
+					const schemaString = JSON.stringify(
+						createGenerator({
+							path,
+							tsconfig: 'tsconfig.json'
+						}).createSchema('Request'),
+						null,
+						'\t'
+					);
+					if (!outputExists) {
+						await fs.createFile(outputPath);
+					}
+					// This is awful. But that's okay because it's funny. Oh, and also useful.
+					await fs.writeFile(
+						outputPath,
+						`import { createValidator } from 'modules/server/api';\n\nexport default createValidator(${schemaString});`
+					);
+					await run(`npx eslint --fix ${outputPath}`);
+				} catch (error) {
+					console.error(error);
 				}
-				// This is awful. But that's okay because it's funny. Oh, and also useful.
-				await fs.writeFile(
-					outputPath,
-					`import { createValidator } from 'modules/server/api';\n\nexport default createValidator(${schemaString});`
-				);
-				await run(`npx eslint --fix ${outputPath}`);
-			} catch (error) {
-				console.error(error);
+				if (!fs.existsSync(path)) {
+					await fs.unlink(outputPath);
+				}
 			}
 		} else if (outputExists) {
 			await fs.unlink(outputPath);
