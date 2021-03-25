c0bce5813977bf26b4cd00164a2c8f8552284b52
GrantGryczan <grantgryczan@gmail.com>
2021-03-25 08:16:26 -0400

    Switch from `jsonschema` to `ajv`

--- modules/server/api.ts
+++ modules/server/api.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Validator } from 'jsonschema';
 import type { IncomingMessage } from 'http';
+import Ajv from 'ajv';
 
 /** The server-side API request object. */
 export type APIRequest<
@@ -30,47 +30,51 @@ export type APIHandler<
 	& { Request?: Request, Response?: Response }
 );
 
-const validator = new Validator();
+const ajv = new Ajv({ allErrors: true });
 
-export const createValidator = (schema: Record<string, unknown>) => (
-	(req: APIRequest<any>, res: APIResponse<any>) => (
+export const createValidator = (schema: Record<string, unknown>) => {
+	const validate = ajv.compile(schema);
+	
+	return (req: APIRequest<any>, res: APIResponse<any>) => (
 		new Promise<void>(resolve => {
-			const result = validator.validate(req, schema, {
-				nestedErrors: true
-			});
+			const valid = validate(req);
 			
-			let methodNotAllowed = false;
-			const errorMessages: string[] = [];
-			
-			for (const error of result.errors) {
-				if (!error.stack.includes('subschema')) {
-					const errorMessage = error.stack.replace(/^instance\./, '').replace(/ a type\(s\) /g, ' type ');
-					if (!errorMessages.includes(errorMessage)) {
-						errorMessages.push(errorMessage);
+			if (valid) {
+				resolve();
+			} else {
+				let methodNotAllowed = false;
+				const errorMessages: string[] = [];
+				
+				for (const error of validate.errors!) {
+					let errorMessage = error.message || 'unknown error';
+					// Filter out unhelpful error messages.
+					if (!errorMessage.includes('schema')) {
+						errorMessage = `${error.dataPath.slice(1).replace(/\//g, '.')}: ${errorMessage}`;
+						if (!errorMessages.includes(errorMessage)) {
+							errorMessages.push(errorMessage);
+						}
+					}
+					
+					// Check if the property which has a schema mismatch is `req.method`.
+					if (error.dataPath === '/method') {
+						methodNotAllowed = true;
+						// This break is an optimization, because once error 405 is detected, we know all other errors are unused and don't need to pushed to the `errors` array.
+						break;
 					}
 				}
 				
-				// Check if the property which has a schema mismatch is `req.method`.
-				if (error.property === 'instance.method') {
-					methodNotAllowed = true;
-					// This break is an optimization, because once error 405 is detected, we know all other errors are unused and don't need to pushed to the `errors` array.
-					break;
+				if (methodNotAllowed) {
+					res.status(405).end();
+				} else {
+					let message = errorMessages.join('\n');
+					if (errorMessages.length > 1) {
+						message = `One or more of the following errors apply:\n${message}`;
+					}
+					res.status(400).send({
+						message
+					});
 				}
-			}
-			
-			if (result.valid) {
-				resolve();
-			} else if (methodNotAllowed) {
-				res.status(405).end();
-			} else {
-				if (errorMessages.length > 1) {
-					const lastIndex = errorMessages.length - 1;
-					errorMessages[lastIndex] = `and/or ${errorMessages[lastIndex]}`;
-				}
-				res.status(400).send({
-					message: `${errorMessages.join(errorMessages.length > 2 ? ',\n' : '\n')}.`
-				});
 			}
 		})
-	)
-);
+	);
+};
