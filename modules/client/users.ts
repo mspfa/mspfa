1c2bd2fe2f20ce4e62907acf71b2cf2d054420ee
GrantGryczan <grantgryczan@gmail.com>
2021-03-29 06:15:39 -0400

    Don't use `Serializable`

--- modules/client/users.ts
+++ modules/client/users.ts
@@ -1,15 +1,17 @@
 import createGlobalState from 'global-react-state';
 import type { UserDocument } from 'modules/server/users';
-import type { Serializable } from 'modules/types';
 
-type ClientUserKey = 'created' | 'lastSeen' | 'birthdate' | 'name' | 'email' | 'verified' | 'description' | 'icon' | 'site' | 'comicSaves' | 'achievements' | 'favs' | 'profileStyle' | 'settings' | 'dev' | 'mod' | 'patron' | 'nameColor';
+type ClientUserDocumentKey = 'name' | 'email' | 'verified' | 'description' | 'icon' | 'site' | 'comicSaves' | 'achievements' | 'favs' | 'profileStyle' | 'settings' | 'dev' | 'mod' | 'patron' | 'nameColor';
 
 /** This is a serializable version of `UserDocument` which only has properties that can safely be exposed to the client that owns the user data. */
 export type ClientUser = (
-	{ id: string }
-	& Serializable<(
-		Pick<UserDocument, ClientUserKey>
-	)>
+	Pick<UserDocument, ClientUserDocumentKey>
+	& {
+		id: string,
+		created: number,
+		lastSeen: number,
+		birthdate: number
+	}
 );
 
 export const [useUser, setUser, getUser] = createGlobalState<ClientUser | undefined>(undefined);
