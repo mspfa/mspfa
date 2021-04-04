fd16faac3ab5eaa5e7be0ebfa12adf0c97f8f2ee
Grant Gryczan <grantgryczan@gmail.com>
2021-04-04 10:36:35 -0400

    Preload Homestuck-Regular font

--- components/Page/index.tsx
+++ components/Page/index.tsx
@@ -9,6 +9,8 @@ export type PageProps = { children: ReactNode };
 
 const Page = ({ children }: PageProps) => (
 	<>
+		{/* This only exists to preload the Homestuck-Regular font so that, later, a fallback font doesn't flicker for a moment while Homestuck-Regular lazy-loads. */}
+		<span id="preload-font" />
 		{/* It is necessary for dialogs to be before the page so that dialog elements are reached first when tabbing. */}
 		<Dialogs />
 		<div id="page">
