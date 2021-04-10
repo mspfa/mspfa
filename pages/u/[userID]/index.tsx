0b6a3ce88e63ea8a0c33b11f308eae50c4c0f4a9
Grant Gryczan <grantgryczan@gmail.com>
2021-04-10 08:27:25 -0400

    Use `withErrorPage` on profile page

--- pages/u/[userID]/index.tsx
+++ pages/u/[userID]/index.tsx
@@ -2,30 +2,32 @@ import Page from 'components/Page';
 import type { MyGetServerSideProps } from 'modules/server/pages';
 import type { PublicUser } from 'modules/client/users';
 import { getUserByUnsafeID, getPublicUser } from 'modules/server/users';
-import ErrorPage from 'pages/_error';
+import { withErrorPage } from 'pages/_error';
 
 type ServerSideProps = {
-	publicUser?: PublicUser
+	publicUser: PublicUser
+} | {
+	statusCode: number
 };
 
-const Component = ({ publicUser }: ServerSideProps) => (
-	publicUser ? (
-		<Page>
-			{publicUser.id}
-		</Page>
-	) : <ErrorPage />
-);
+const Component = withErrorPage<ServerSideProps>(({ publicUser }) => (
+	<Page>
+		{publicUser.id}
+	</Page>
+));
 
 export default Component;
 
 export const getServerSideProps: MyGetServerSideProps<ServerSideProps> = async context => {
-	const props: ServerSideProps = {};
-
 	const userFromParams = await getUserByUnsafeID(context.params.userID);
 
 	if (userFromParams) {
-		props.publicUser = getPublicUser(userFromParams);
+		return {
+			props: {
+				publicUser: getPublicUser(userFromParams)
+			}
+		};
 	}
 
-	return { props };
+	return { props: { statusCode: 404 } };
 };
