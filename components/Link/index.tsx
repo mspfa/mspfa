bb1c845aa07c891f9563c3b56ba65fb6b56e10ab
Grant Gryczan <grantgryczan@gmail.com>
2021-05-01 05:12:39 -0400

    Allow `<Link className="button">`

--- components/Link/index.tsx
+++ components/Link/index.tsx
@@ -6,11 +6,11 @@ import type { LinkProps as OriginalNextLinkProps } from 'next/link';
 import React from 'react';
 import type { AnchorHTMLAttributes } from 'react';
 
-// `href` is omitted here because NextLinkProps has a more inclusive `href`, accepting URL objects in addition to strings.
+// `href` is omitted here because `NextLinkProps` has a more inclusive `href`, accepting URL objects in addition to strings.
 type HTMLAnchorProps = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>;
 // `passHref` is omitted here because it is not useful enough to be worth implementing.
 type NextLinkProps = Omit<OriginalNextLinkProps, 'passHref'>;
-// NextLinkProps is `Partial`ed in LinkProps below to make `href` optional in the Link component. NextLinkProps above is not `Partial`ed because `href` is required in NextLink's props.
+// `NextLinkProps` is `Partial`ed in `LinkProps` below to make `href` optional in the Link component. `NextLinkProps` above is not `Partial`ed because `href` is required in `NextLink`'s props.
 export type LinkProps = HTMLAnchorProps & Partial<NextLinkProps>;
 
 /**
@@ -23,7 +23,7 @@ export type LinkProps = HTMLAnchorProps & Partial<NextLinkProps>;
  */
 const Link = React.forwardRef<HTMLAnchorElement & HTMLButtonElement, LinkProps>((
 	{
-		// All NextLink-exclusive props.
+		// All `NextLink`-exclusive props.
 		as,
 		prefetch,
 		replace,
@@ -31,14 +31,20 @@ const Link = React.forwardRef<HTMLAnchorElement & HTMLButtonElement, LinkProps>(
 		shallow,
 		locale,
 
-		// All non-NextLink-exclusive props.
+		// All non-`NextLink`-exclusive props.
 		className,
 		href,
 		...props
 	},
 	ref
 ) => {
-	const linkClassName = `link${className ? ` ${className}` : ''}`;
+	const linkClassName = (
+		className
+			? /(^| )button( |$)/.test(className)
+				? className
+				: `link ${className}`
+			: 'link'
+	);
 
 	if (href === undefined) {
 		return (
@@ -57,11 +63,11 @@ const Link = React.forwardRef<HTMLAnchorElement & HTMLButtonElement, LinkProps>(
 	const anchorProps: Omit<LinkProps, 'href'> & { href?: string } = {
 		...props,
 		className: linkClassName,
-		// Anchors don't accept URL objects like NextLink's `href` prop does, so in case `href` is a URL object, it should be overwritten with the string version in `anchorProps`.
+		// Anchors don't accept URL objects like `NextLink`'s `href` prop does, so in case `href` is a URL object, it should be overwritten with the string version in `anchorProps`.
 		href: hrefString
 	};
 
-	// NextLinks aren't useful for external links, so if the link is external, just return the anchor.
+	// `NextLink`s aren't useful for external links, so if the link is external, just return the anchor.
 	if (external) {
 		// If the link should open in a new tab, add `noreferrer noopener` to its `rel` since you can't trust external targets with [`window.opener`](https://developer.mozilla.org/en-US/docs/Web/API/Window/opener) on older browsers.
 		if (anchorProps.target === '_blank') {
@@ -78,7 +84,7 @@ const Link = React.forwardRef<HTMLAnchorElement & HTMLButtonElement, LinkProps>(
 		return <a {...anchorProps} ref={ref} />;
 	}
 
-	// Otherwise, if the link is not external, wrap the anchor in a NextLink to get those [nice features](https://nextjs.org/docs/api-reference/next/link).
+	// Otherwise, if the link is not external, wrap the anchor in a `NextLink` to get those [nice features](https://nextjs.org/docs/api-reference/next/link).
 	return (
 		<NextLink
 			href={href}
