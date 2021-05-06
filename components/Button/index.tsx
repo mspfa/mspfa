990c8794b9dfc8609bb7ef96fcd5b31dcb54f77c
Grant Gryczan <grantgryczan@gmail.com>
2021-05-06 14:30:20 -0400

    Add info about `Link` buttons to `Button` comment

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -4,7 +4,11 @@ import type { ButtonHTMLAttributes } from 'react';
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-/** A styled `button` element. Accepts any props which `button` accepts, except `type` which is replaced with the `submit?: boolean` prop. */
+/**
+ * A styled `button` element. Accepts any props which `button` accepts, except `type` which is replaced with the `submit?: boolean` prop.
+ *
+ * If you want a styled button to function as a `Link`, use `<Link className="button">` instead.
+ */
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>((
 	{
 		type = 'button',
