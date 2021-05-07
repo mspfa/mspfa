717c07eb327d6634939e8d65f628afbcb36af8b7
Grant Gryczan <grantgryczan@gmail.com>
2021-05-07 03:15:52 -0400

    Display error messages in production

--- modules/client/global.ts
+++ modules/client/global.ts
@@ -1,5 +1,22 @@
 // This script is executed once initially on the client.
 
 import * as MSPFA from 'modules/client/MSPFA';
+import { Dialog } from 'modules/client/dialogs';
+import env from 'modules/client/env';
 
 (global as any).MSPFA = MSPFA;
+
+window.addEventListener('error', event => {
+	if (env.NODE_ENV === 'development' || /bot/i.test(navigator.userAgent)) {
+		return;
+	}
+
+	if (event.filename.startsWith(`${location.origin}/`)) {
+		new Dialog({
+			title: 'Uncaught Error',
+			content: `${event.message}\n\n${event.error.stack || (
+				`${event.error.message}\n    at ${event.filename}:${event.lineno}${event.colno ? `:${event.colno}` : ''}`
+			)}`
+		});
+	}
+});
