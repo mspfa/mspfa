8169b6afb02d29bde354a680c3f84755327a329c
Grant Gryczan <grantgryczan@gmail.com>
2021-05-09 08:29:58 -0400

    Styled errors

--- modules/client/global.tsx
+++ modules/client/global.tsx
@@ -8,19 +8,25 @@ import env from 'modules/client/env';
 
 window.addEventListener('error', event => {
 	if (env.NODE_ENV === 'development' || /bot/i.test(navigator.userAgent)) {
-		return;
+		// return;
 	}
 
 	if (event.filename.startsWith(`${location.origin}/`)) {
 		new Dialog({
 			title: 'Uncaught Error',
-			content: `${
-				event.message
-			}\n\n${
-				event.error.stack || (
-					`${event.error.message}\n    at ${event.filename}:${event.lineno}${event.colno ? `:${event.colno}` : ''}`
-				)
-			}`
+			content: (
+				<>
+					<div className="error">
+						{event.message}
+					</div>
+					<br />
+					<div className="translucent-text">
+						{event.error.stack || (
+							`${event.error.message}\n    at ${event.filename}:${event.lineno}${event.colno ? `:${event.colno}` : ''}`
+						)}
+					</div>
+				</>
+			)
 		});
 	}
 });
