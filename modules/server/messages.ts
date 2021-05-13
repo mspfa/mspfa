ff1c59df4097725c3cb54a1e028cf59e42dd29f1
Grant Gryczan <grantgryczan@gmail.com>
2021-05-13 17:50:35 -0400

    Set more message schema requirements

--- modules/server/messages.ts
+++ modules/server/messages.ts
@@ -7,14 +7,21 @@ export type MessageDocument = {
 	sent: Date,
 	edited?: Date,
 	from: UserDocument['_id'],
+	/** @minItems 1 */
 	to: Array<UserDocument['_id']>,
 	/** The message ID which this is a reply to, or undefined if it is not a reply. */
 	replyTo?: MessageDocument['_id'],
 	notDeletedBy: Array<UserDocument['_id']>,
 	notReadBy: Array<UserDocument['_id']>,
-	/** @maxLength 50 */
+	/**
+	 * @minLength 1
+	 * @maxLength 50
+	 */
 	subject: string,
-	/** @maxLength 20000 */
+	/**
+	 * @minLength 1
+	 * @maxLength 20000
+	 */
 	content: string
 };
 
