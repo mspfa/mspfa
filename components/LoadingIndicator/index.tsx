70ce53cc660a8e86f68db797e173f1184276ffdd
Grant Gryczan <grantgryczan@gmail.com>
2021-05-13 16:01:48 -0400

    Show the loading indicator after 250 ms

--- components/LoadingIndicator/index.tsx
+++ components/LoadingIndicator/index.tsx
@@ -5,6 +5,10 @@ import Router from 'next/router';
 
 const [useLoadingCount, setLoadingCount, getLoadingCount] = createGlobalState(0);
 
+/** An amount to be set as loading count at the end of the `startLoadingTimeout`. */
+let queuedLoadingCount = 0;
+let startLoadingTimeout: NodeJS.Timeout | undefined;
+
 /**
  * Increment the count which displays the loading indicator when non-zero.
  *
@@ -14,9 +18,26 @@ export const startLoading = (
 	/** The number of things which started loading. */
 	count = 1
 ) => {
-	const loadingCount = getLoadingCount() + count;
-	setLoadingCount(loadingCount);
-	return loadingCount;
+	if (startLoadingTimeout) {
+		return queuedLoadingCount += count;
+	}
+
+	const previousLoadingCount = getLoadingCount();
+
+	if (previousLoadingCount) {
+		const loadingCount = previousLoadingCount + count;
+		setLoadingCount(loadingCount);
+		return loadingCount;
+	}
+
+	startLoadingTimeout = setTimeout(() => {
+		startLoadingTimeout = undefined;
+
+		setLoadingCount(queuedLoadingCount);
+		queuedLoadingCount = 0;
+	}, 250);
+
+	return queuedLoadingCount += count;
 };
 
 /**
@@ -27,37 +48,31 @@ export const startLoading = (
 export const stopLoading = (
 	/** The number of things which stopped loading. */
 	count = 1
-) => startLoading(-count);
-
-let loadingPage = false;
-let startLoadingTimeout: NodeJS.Timeout | undefined;
-
-Router.events.on('routeChangeStart', () => {
+) => {
 	if (startLoadingTimeout) {
-		// Not sure if this is actually necessary--I've not been able to get this to call from my testing--but might as well put it here.
-		clearTimeout(startLoadingTimeout);
+		queuedLoadingCount -= count;
+
+		if (!queuedLoadingCount) {
+			clearTimeout(startLoadingTimeout);
+			startLoadingTimeout = undefined;
+		}
+
+		return;
 	}
 
-	// Only show the loading indicator if the route change takes at least this timeout's duration to load.
-	startLoadingTimeout = setTimeout(() => {
-		startLoadingTimeout = undefined;
+	const loadingCount = getLoadingCount() - count;
+	setLoadingCount(loadingCount);
+	return loadingCount;
+};
 
-		startLoading();
-		loadingPage = true;
-	}, 250);
+Router.events.on('routeChangeStart', () => {
+	startLoading();
 });
 
 const stopLoadingPage = () => {
 	// This `setTimeout` is necessary so the loading state of the `LoadingIndicator` component is not set during a route change, causing it to update incorrectly.
 	setTimeout(() => {
-		if (loadingPage) {
-			loadingPage = false;
-			stopLoading();
-		} else {
-			clearTimeout(startLoadingTimeout!);
-
-			startLoadingTimeout = undefined;
-		}
+		stopLoading();
 	});
 };
 Router.events.on('routeChangeComplete', stopLoadingPage);
