4bc0dffa2874165704e4fa678b66bf397904c280
Grant Gryczan <grantgryczan@gmail.com>
2021-05-18 22:45:35 -0400

    Render message metadata

--- pages/messages/[messageID]/index.tsx
+++ pages/messages/[messageID]/index.tsx
@@ -7,24 +7,63 @@ import { Perm } from 'modules/client/perms';
 import { getClientMessage, getMessageByUnsafeID } from 'modules/server/messages';
 import type { ClientMessage } from 'modules/client/messages';
 import BBCode from 'components/BBCode';
+import type { UserDocument } from 'modules/server/users';
+import users, { getPublicUser } from 'modules/server/users';
+import type { PublicUser } from 'modules/client/users';
+import { uniqBy } from 'lodash';
+import { useUserCache } from 'modules/client/UserCache';
+import Link from 'components/Link';
+import { Fragment } from 'react';
+import Timestamp from 'components/Timestamp';
 
 type ServerSideProps = {
-	message: ClientMessage
+	message: ClientMessage,
+	userCache: PublicUser[]
 } | {
 	statusCode: number
 };
 
-const Component = withErrorPage<ServerSideProps>(({ message }) => (
-	<Page flashyTitle heading="Message">
-		<Box>
-			<BoxSection heading={message.subject}>
-				<BBCode html>
-					{message.content}
-				</BBCode>
-			</BoxSection>
-		</Box>
-	</Page>
-));
+const Component = withErrorPage<ServerSideProps>(({ message, userCache: initialUserCache }) => {
+	const { cacheUser, userCache } = useUserCache();
+	initialUserCache.forEach(cacheUser);
+
+	const fromUser = userCache[message.from]!;
+	const toUsers = message.to.map(userID => userCache[userID]!);
+
+	return (
+		<Page flashyTitle heading="Message">
+			<Box>
+				<BoxSection
+					id="message-meta"
+					heading={message.subject}
+				>
+					{'From: '}
+					<Link href={`/u/${fromUser.id}`}>
+						{fromUser.name}
+					</Link><br />
+					{'To: '}
+					{toUsers.map((toUser, index) => (
+						<Fragment key={toUser.id}>
+							{index !== 0 && ', '}
+							<Link href={`/u/${toUser.id}`}>
+								{toUser.name}
+							</Link>
+						</Fragment>
+					))}<br />
+					{'Date Sent: '}
+					<Timestamp withTime>
+						{message.sent}
+					</Timestamp>
+				</BoxSection>
+				<BoxSection>
+					<BBCode html>
+						{message.content}
+					</BBCode>
+				</BoxSection>
+			</Box>
+		</Page>
+	);
+});
 
 export default Component;
 
@@ -37,7 +76,7 @@ export const getServerSideProps = withStatusCode<ServerSideProps>(async ({ req, 
 
 	if (!(
 		req.user && (
-			req.user._id.equals(messageFromParams.from)
+			messageFromParams.from.equals(req.user._id)
 			|| messageFromParams.to.some(userID => userID.equals(req.user!._id))
 			|| req.user.perms & Perm.sudoRead
 		)
@@ -45,9 +84,20 @@ export const getServerSideProps = withStatusCode<ServerSideProps>(async ({ req, 
 		return { props: { statusCode: 403 } };
 	}
 
+	const userCacheIDs = uniqBy([messageFromParams.from, ...messageFromParams.to], String);
+
 	return {
 		props: {
-			message: getClientMessage(messageFromParams)
+			message: getClientMessage(messageFromParams),
+			userCache: (
+				(
+					(
+						await Promise.all(userCacheIDs.map(
+							userID => users.findOne({ _id: userID })
+						))
+					).filter(Boolean) as UserDocument[]
+				).map(getPublicUser)
+			)
 		}
 	};
 });
