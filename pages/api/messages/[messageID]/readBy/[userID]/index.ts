aa6cba7254ac09ea544f5af42e40bd90cb8278a1
Grant Gryczan <grantgryczan@gmail.com>
2021-05-24 22:47:08 -0400

    API for marking messages as unread

---
+++ pages/api/messages/[messageID]/readBy/[userID]/index.ts
@@ -0,0 +1,45 @@
+import validate from './index.validate';
+import type { APIHandler } from 'modules/server/api';
+import messages, { getMessageByUnsafeID } from 'modules/server/messages';
+import { Perm } from 'modules/client/perms';
+import { permToGetUserInAPI } from 'modules/server/perms';
+
+const Handler: APIHandler<{
+	query: {
+		messageID: string,
+		userID: string
+	},
+	method: 'DELETE'
+}> = async (req, res) => {
+	await validate(req, res);
+
+	const user = await permToGetUserInAPI(req, res, Perm.sudoWrite);
+
+	const message = await getMessageByUnsafeID(req.query.messageID, res);
+
+	if (!message.notDeletedBy.some(userID => userID.equals(user._id))) {
+		res.status(422).send({
+			message: 'The specified user does not have permission to mark the specified message as unread.'
+		});
+		return;
+	}
+
+	if (message.notReadBy.some(userID => userID.equals(user._id))) {
+		res.status(404).send({
+			message: 'The specified user has already marked the specified message as unread.'
+		});
+		return;
+	}
+
+	await messages.updateOne({
+		_id: message._id
+	}, {
+		$push: {
+			notReadBy: user._id
+		}
+	});
+
+	res.end();
+};
+
+export default Handler;
