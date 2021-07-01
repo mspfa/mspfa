3d8d885f832026287eca993e1a48b46f09120f50
Grant Gryczan <grantgryczan@gmail.com>
2021-07-01 16:01:28 -0400

    Improve page editor boilerplate

--- pages/s/[storyID]/edit/p/index.tsx
+++ pages/s/[storyID]/edit/p/index.tsx
@@ -4,20 +4,20 @@ import { Perm } from 'modules/client/perms';
 import { withErrorPage } from 'modules/client/errors';
 import { withStatusCode } from 'modules/server/errors';
 import { Form, Formik } from 'formik';
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { useLeaveConfirmation } from 'modules/client/forms';
 import Box from 'components/Box';
-import BoxFooter from 'components/Box/BoxFooter';
 import Button from 'components/Button';
 import { getPrivateStory, getStoryByUnsafeID } from 'modules/server/stories';
 import type { PrivateStory } from 'modules/client/stories';
 import BoxSection from 'components/Box/BoxSection';
 import type { APIClient } from 'modules/client/api';
+import Row from 'components/Row';
 
 type StoryAPI = APIClient<typeof import('pages/api/stories/[storyID]').default>;
 
 const getValuesFromStory = (privateStory: PrivateStory) => ({
-
+	pages: []
 });
 
 type Values = ReturnType<typeof getValuesFromStory>;
@@ -32,6 +32,8 @@ const Component = withErrorPage<ServerSideProps>(({ privateStory: initialPrivate
 	const [privateStory, setPrivateStory] = useState(initialPrivateStory);
 
 	const initialValues = getValuesFromStory(privateStory);
+
+	const notifyCheckboxRef = useRef<HTMLInputElement>(null!);
 
 	return (
 		<Page heading="Edit Adventure">
@@ -50,34 +52,72 @@ const Component = withErrorPage<ServerSideProps>(({ privateStory: initialPrivate
 					return (
 						<Form>
 							<Box>
-								<BoxSection heading={privateStory.title}>
-									hello world
+								<BoxSection
+									id="editor-options"
+									heading={privateStory.title}
+								>
+									<Row>
+										<Button
+											className="small"
+											href={`/s/${privateStory.id}/edit`}
+										>
+											Edit Adventure
+										</Button>
+										<Button className="small">Jump to Page</Button>
+										<Button className="small">Find and Replace</Button>
+									</Row>
+									<Row>
+										<label>
+											<input
+												type="checkbox"
+												className="spaced"
+												ref={notifyCheckboxRef}
+											/>
+											<span className="spaced">
+												Notify readers of newly published pages during this editing session
+											</span>
+										</label>
+									</Row>
 								</BoxSection>
-								<BoxFooter>
-									<Button href={`/s/${privateStory.id}/edit`}>
-										Edit Adventure
-									</Button>
-									<Button
-										type="submit"
-										className="alt"
-										disabled={!dirty || isSubmitting}
-									>
-										Save
-									</Button>
-								</BoxFooter>
 							</Box>
+							<Row id="story-page-actions">
+								<Button>
+									New Page
+								</Button>
+								<Button
+									type="submit"
+									className="alt"
+									disabled={!dirty || isSubmitting}
+								>
+									Save All
+								</Button>
+								<Button
+									className="alt"
+									disabled={true || dirty || isSubmitting}
+								>
+									Publish
+								</Button>
+							</Row>
 							<Box id="story-page-box">
-								<BoxSection className="story-page-section" heading="Page 2">
-									awdawdawdawda
-								</BoxSection>
-								<BoxSection className="story-page-section" heading="Page 1">
-									awdawdawdawda
-								</BoxSection>
-								<BoxFooter>
+								<Row id="story-page-sections">
+									<BoxSection
+										className="story-page-section"
+										heading="Page 2"
+									>
+										awdawdawdawda
+									</BoxSection>
+									<BoxSection
+										className="story-page-section"
+										heading="Page 1"
+									>
+										awdawdawdawda
+									</BoxSection>
+								</Row>
+								<Row id="story-page-view-actions">
 									<Button>
 										More
 									</Button>
-								</BoxFooter>
+								</Row>
 							</Box>
 						</Form>
 					);
