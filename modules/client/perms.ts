79298ec26b6b7dc98314de0669ed769fd48a55b6
Grant Gryczan <grantgryczan@gmail.com>
2021-07-10 18:35:25 -0400

    Correct `Perm` comments

--- modules/client/perms.ts
+++ modules/client/perms.ts
@@ -1,9 +1,9 @@
 export enum Perm {
 	/** Permission to grant or revoke perms for users (including yourself). */
 	writePerms = 0b1,
-	/** Permission to access (but not edit or delete) things which at least one normal client can access. */
+	/** Permission to access (but not necessarily edit or delete) things which at least one normal client can access. */
 	sudoRead = 0b10,
-	/** Permission to edit (but not delete) things which at least one normal client can edit. */
+	/** Permission to edit (but not necessarily delete) things which at least one normal client can edit. */
 	sudoWrite = 0b100,
 	/** Permission to delete things which at least one normal client can delete, and permission to ban users. */
 	sudoDelete = 0b1000,
