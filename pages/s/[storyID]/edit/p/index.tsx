f4d6580223defc9708d3cc4ca4b6349645c9d0f2
Grant Gryczan <grantgryczan@gmail.com>
2021-07-23 10:20:21 -0400

    Add hover text to View Mode button

--- pages/s/[storyID]/edit/p/index.tsx
+++ pages/s/[storyID]/edit/p/index.tsx
@@ -619,6 +619,7 @@ const Component = withErrorPage<ServerSideProps>(({
 										</Button>
 										<Button
 											className="small"
+											title={`Click to Set View Mode to ${viewMode === 'grid' ? 'List' : 'Grid'}`}
 											disabled={!viewMode || formikPropsRef.current.isSubmitting}
 											onClick={
 												useCallback(() => {
