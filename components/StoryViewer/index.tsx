afc4ee24d09e3656473848efcf70391d51b35144
Grant Gryczan <grantgryczan@gmail.com>
2021-08-02 02:19:28 -0400

    Server-side initial page caching

--- components/StoryViewer/index.tsx
+++ components/StoryViewer/index.tsx
@@ -1,14 +1,70 @@
 import Page from 'components/Page';
-import type { PublicStory } from 'modules/client/stories';
+import type { ClientStoryPage, PublicStory } from 'modules/client/stories';
+import Link from 'components/Link';
+import { useRouter } from 'next/router';
+import type { ReactNode } from 'react';
+import { useState, useEffect } from 'react';
+import type { StoryPageID } from 'modules/server/stories';
+import BBCode from 'components/BBCode';
+
+/**
+ * The number of next pages to preload ahead of the user's current page.
+ *
+ * For example, if this is 2 and the user is on page 10, then pages 10 to 12 will be preloaded (assuming page 10 only links to page 11 and page 11 only links to page 12).
+ */
+export const PAGE_PRELOAD_DEPTH = 5;
 
 export type StoryViewerProps = {
-	story: PublicStory
+	story: PublicStory,
+	/**
+	 * The initial record of pages to cache.
+	 *
+	 * If a page ID maps to `null`, then the page does not exist to the client, letting the client know not to try to request it.
+	 */
+	pages: Partial<Record<StoryPageID, ClientStoryPage | null>>
 };
 
-const StoryViewer = ({ story }: StoryViewerProps) => (
-	<Page>
-		{story.title}
-	</Page>
-);
+const StoryViewer = ({
+	story,
+	pages: initialPages
+}: StoryViewerProps) => {
+	const router = useRouter();
+	const pageID = (
+		typeof router.query.p === 'string'
+			? +router.query.p
+			: 1
+	);
+
+	// This state is record of cached pages.
+	// If a page ID maps to `null`, then the page does not exist to this client, letting it know not to try to request it.
+	// If a page ID maps to `undefined`, then it has not been cached or requested yet.
+	const [pages, setPages] = useState(initialPages);
+	const page = pages[pageID];
+
+	// This state is a ref to a partial record that maps each preloaded page ID to a `ReactNode` of its parsed `content`, as a caching optimization due to the cost of BBCode parsing.
+	const [pageContents, setPageContents] = useState<Partial<Record<StoryPageID, ReactNode>>>(() => (
+		page
+			? {
+				[pageID]: BBCode({
+					html: true,
+					children: page.content
+				})
+			}
+			: {}
+	));
+
+	// Cache new pages and preload the BBCode and images of cached pages.
+	// None of this should be done server-side, because anything cached or preloaded server-side would be unnecessary as it would not be sent to the client.
+	useEffect(() => {
+
+	}, [pageID, pages]);
+
+	return (
+		<Page>
+			{story.title}<br />
+			{pageID}
+		</Page>
+	);
+};
 
 export default StoryViewer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
+import { PAGE_PRELOAD_DEPTH } from 'components/StoryViewer';
 import { uniqBy } from 'lodash';
 import { withErrorPage } from 'modules/client/errors';
 import { Perm } from 'modules/client/perms';
-import type { PublicStory } from 'modules/client/stories';
+import type { ClientStoryPage, PublicStory } from 'modules/client/stories';
 import { StoryPrivacy } from 'modules/client/stories';
+import { useUserCache } from 'modules/client/UserCache';
 import type { PublicUser } from 'modules/client/users';
 import { withStatusCode } from 'modules/server/errors';
-import { getPublicStory, getStoryByUnsafeID } from 'modules/server/stories';
+import type { ServerStoryPage, StoryPageID } from 'modules/server/stories';
+import { getClientStoryPage, getPublicStory, getStoryByUnsafeID } from 'modules/server/stories';
 import users, { getPublicUser } from 'modules/server/users';
 import dynamic from 'next/dynamic';
 
@@ -13,20 +16,38 @@ const Homepage = dynamic(() => import('components/Homepage'));
 const StoryViewer = dynamic(() => import('components/StoryViewer'));
 
 type ServerSideProps = {
+	userCache?: never,
 	publicStory?: never,
-	userCache?: never
+	pages?: never
 } | {
+	userCache: PublicUser[],
 	publicStory: PublicStory,
-	userCache: PublicUser[]
+	pages: Record<StoryPageID, ClientStoryPage | null>
 } | {
 	statusCode: number
 };
 
-const Component = withErrorPage<ServerSideProps>(({ publicStory }) => (
-	publicStory
-		? <StoryViewer story={publicStory} />
-		: <Homepage />
-));
+const Component = withErrorPage<ServerSideProps>(({
+	userCache: initialUserCache,
+	publicStory,
+	pages
+}) => {
+	const { cacheUser } = useUserCache();
+	initialUserCache?.forEach(cacheUser);
+
+	return (
+		publicStory
+			? (
+				<StoryViewer
+					// This key is to make the `StoryViewer`'s states not preserve between different stories.
+					key={publicStory.id}
+					story={publicStory}
+					pages={pages!}
+				/>
+			)
+			: <Homepage />
+	);
+});
 
 export default Component;
 
@@ -45,25 +66,90 @@ export const getServerSideProps = withStatusCode<ServerSideProps>(async ({ req, 
 		return { props: { statusCode: 404 } };
 	}
 
-	if (story.privacy === StoryPrivacy.Private && !(
-		req.user && (
+	const readPerms = (
+		!!req.user && (
 			story.owner.equals(req.user._id)
 			|| story.editors.some(userID => userID.equals(req.user!._id))
-			|| req.user.perms & Perm.sudoRead
+			|| !!(req.user.perms & Perm.sudoRead)
 		)
-	)) {
+	);
+
+	if (story.privacy === StoryPrivacy.Private && !readPerms) {
 		return { props: { statusCode: 403 } };
 	}
 
+	const pageID = (
+		typeof query.p === 'string'
+			? +query.p
+			: 1
+	);
+
+	/**
+	 * A record of pages to send to the client.
+	 *
+	 * If a page ID maps to `null`, then the page does not exist to the client, letting the client know not to try to request it.
+	 */
+	const clientPages: Record<StoryPageID, ClientStoryPage | null> = {};
+
+	const now = Date.now();
+
+	/** Whether this user is in preview mode (which shows unpublished pages) and has permission to be in preview mode. */
+	const previewMode = 'preview' in query && readPerms;
+
+	/** Adds pages to `clientPages`, doing the same for their `nextPages` recursively until the recursion depth reaches the `PAGE_PRELOAD_DEPTH`. */
+	const addToClientPages = (
+		/** The the ID of the page to add to `clientPages`. */
+		pageID: StoryPageID,
+		/** The recursion depth of this function call. */
+		depth: number
+	) => {
+		// If this page is already added, then don't continue.
+		if (pageID in clientPages) {
+			return;
+		}
+
+		// This is asserted as nullable because `pageID` may not index a real page.
+		const serverPage = story.pages[pageID] as ServerStoryPage | undefined;
+
+		// If this page doesn't exist or the user doesn't have access to it, then set it to `null` and don't continue.
+		if (!serverPage || (
+			!previewMode && (
+				// Check if this page is not public.
+				serverPage.published === undefined || +serverPage.published > now
+			)
+		)) {
+			clientPages[pageID] = null;
+			return;
+		}
+
+		const clientPage = getClientStoryPage(story.pages[pageID]);
+
+		// Add the `clientPage` to `clientPages`.
+		clientPages[pageID] = clientPage;
+
+		if (++depth > PAGE_PRELOAD_DEPTH) {
+			// If this iteration has reached the `PAGE_PRELOAD_DEPTH`, don't iterate any deeper.
+			return;
+		}
+
+		// Call this function of each of this page's `nextPages` as well.
+		for (const nextPageID of clientPage.nextPages) {
+			addToClientPages(nextPageID, depth);
+		}
+	};
+
+	addToClientPages(pageID, 0);
+
 	return {
 		props: {
-			publicStory: getPublicStory(story),
 			userCache: await users.find!({
 				_id: {
 					$in: uniqBy([story.owner, ...story.editors], String)
 				},
 				willDelete: { $exists: false }
-			}).map(getPublicUser).toArray()
+			}).map(getPublicUser).toArray(),
+			publicStory: getPublicStory(story),
+			pages: clientPages
 		}
 	};
 });
