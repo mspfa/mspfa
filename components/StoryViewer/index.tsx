72adb7468e3db8df06ba31c5004bbf476256c2c2
Grant Gryczan <grantgryczan@gmail.com>
2021-08-02 03:31:57 -0400

    Cache sanitized page contents

--- components/StoryViewer/index.tsx
+++ components/StoryViewer/index.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router';
 import type { ReactNode } from 'react';
 import { useState, useEffect } from 'react';
 import type { StoryPageID } from 'modules/server/stories';
-import BBCode from 'components/BBCode';
+import BBCode, { sanitizeBBCode } from 'components/BBCode';
 
 /**
  * The number of next pages to preload ahead of the user's current page.
@@ -13,6 +13,9 @@ import BBCode from 'components/BBCode';
  * For example, if this is 2 and the user is on page 10, then pages 10 to 12 will be preloaded (assuming page 10 only links to page 11 and page 11 only links to page 12).
  */
 export const PAGE_PRELOAD_DEPTH = 5;
+
+/** Returns an HTML string of the sanitized `page.content` BBCode. */
+const sanitizePageContent = (page: ClientStoryPage) => sanitizeBBCode(page.content, { html: true });
 
 export type StoryViewerProps = {
 	story: PublicStory,
@@ -37,32 +40,41 @@ const StoryViewer = ({
 
 	// This state is record of cached pages.
 	// If a page ID maps to `null`, then the page does not exist to this client, letting it know not to try to request it.
-	// If a page ID maps to `undefined`, then it has not been cached or requested yet.
+	// If a page ID's value is undefined, then it has not been cached or requested yet.
 	const [pages, setPages] = useState(initialPages);
 	const page = pages[pageID];
 
-	// This state is a ref to a partial record that maps each preloaded page ID to a `ReactNode` of its parsed `content`, as a caching optimization due to the cost of BBCode parsing.
-	const [pageContents, setPageContents] = useState<Partial<Record<StoryPageID, ReactNode>>>(() => (
-		page
-			? {
-				[pageID]: BBCode({
-					html: true,
-					children: page.content
-				})
-			}
-			: {}
-	));
+	// This state is a ref to a partial record that maps each cached page ID to an HTML string of its sanitized `content`, as a caching optimization due to the performance cost of BBCode sanitization.
+	const [pageContents, setPageContents] = useState<Partial<Record<StoryPageID, string>>>({});
+	// Mutate `pageContents` to load this page's sanitized contents synchronously.
+	if (page && !pageContents[pageID]) {
+		pageContents[pageID] = sanitizePageContent(page);
+	}
 
 	// Cache new pages and preload the BBCode and images of cached pages.
 	// None of this should be done server-side, because anything cached or preloaded server-side would be unnecessary as it would not be sent to the client.
 	useEffect(() => {
+		let pageContentsChanged = false;
+		const newPageContents = { ...pageContents };
 
-	}, [pageID, pages]);
+		for (const page of Object.values(pages)) {
+			if (page && !(pageID in newPageContents)) {
+				newPageContents[pageID] = sanitizePageContent(page);
+				pageContentsChanged = true;
+			}
+		}
+
+		if (pageContentsChanged) {
+			setPageContents(newPageContents);
+		}
+	}, [pageID, pages, pageContents]);
 
 	return (
 		<Page>
 			{story.title}<br />
-			{pageID}
+			<BBCode alreadySanitized>
+				{pageContents[pageID]}
+			</BBCode>
 		</Page>
 	);
 };
