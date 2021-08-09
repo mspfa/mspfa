d34f77ac4336bf061fc8e0ea8460749ad04cf016
Grant Gryczan <grantgryczan@gmail.com>
2021-08-08 23:30:10 -0400

    `useMobile` hook

---
+++ modules/client/useMobile.ts
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { useIsomorphicLayoutEffect } from 'react-use';
+
+const PAGE_WIDTH = 950;
+
+/** A hook which returns whether the user is on mobile. */
+export const useMobile = (defaultValue = false) => {
+	const [mobile, setMobile] = useState(defaultValue);
+
+	// This is a layout effect hook rather than a normal effect hook so that `mobile` can hold the correct value as soon as possible.
+	useIsomorphicLayoutEffect(() => {
+		/** A media query which matches whether the user is on mobile. */
+		const mobileQuery = window.matchMedia(`not screen and (min-width: ${PAGE_WIDTH}px)`);
+
+		const updateMobileQuery = () => {
+			if (mobile !== mobileQuery.matches) {
+				setMobile(mobileQuery.matches);
+			}
+		};
+
+		mobileQuery.addEventListener('change', updateMobileQuery);
+
+		updateMobileQuery();
+
+		return () => {
+			mobileQuery.removeEventListener('change', updateMobileQuery);
+		};
+	}, [mobile]);
+
+	return mobile;
+};
