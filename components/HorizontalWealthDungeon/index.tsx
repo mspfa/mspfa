c091a4b9adf30b6c8eee97d03120a1e803fe4cb6
Grant Gryczan <grantgryczan@gmail.com>
2021-08-12 01:13:01 -0400

    Remove background color from wealth spawner cage

--- components/HorizontalWealthDungeon/index.tsx
+++ components/HorizontalWealthDungeon/index.tsx
@@ -2,7 +2,7 @@ import './styles.module.scss';
 
 const HorizontalWealthDungeon = () => (
 	<div className="wealth-dungeon horizontal">
-		<div className="wealth-spawner-cage front">
+		<div className="wealth-spawner-cage">
 			{/* TODO: Insert wealth spawner here. */}
 		</div>
 	</div>
