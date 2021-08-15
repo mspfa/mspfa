cdbb491242dfffda3061c971b82f2c0197ee6099
Grant Gryczan <grantgryczan@gmail.com>
2021-08-15 19:52:56 -0400

    Don't update `FlashyTitle` on query change

--- components/FlashyTitle/index.tsx
+++ components/FlashyTitle/index.tsx
@@ -16,12 +16,20 @@ const flashyTitleColors = [
 const getFlashyTitleColor = () => flashyTitleColors[Math.floor(Math.random() * flashyTitleColors.length)];
 
 const FlashyTitle = () => {
-	const router = useRouter();
 	const [color, setColor] = useState(getFlashyTitleColor);
+
+	const router = useRouter();
+	const asPathQueryIndex = router.asPath.indexOf('?');
+	const asPathname = (
+		asPathQueryIndex === -1
+			? router.asPath
+			// Slice off the query so the effect hook isn't cycled when the query changes.
+			: router.asPath.slice(0, asPathQueryIndex)
+	);
 
 	useIsomorphicLayoutEffect(() => () => {
 		setColor(getFlashyTitleColor());
-	}, [router.asPath]);
+	}, [asPathname]);
 
 	return (
 		<div id="flashy-title-container">
