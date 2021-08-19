bf6e9a8398e9510c108d31269a2e4e6912ea7805
Grant Gryczan <grantgryczan@gmail.com>
2021-08-19 16:37:33 -0400

    Forward `Row` ref

--- components/Row/index.tsx
+++ components/Row/index.tsx
@@ -1,14 +1,19 @@
 import './styles.module.scss';
 import type { HTMLAttributes } from 'react';
+import React from 'react';
 
 export type RowProps = HTMLAttributes<HTMLDivElement>;
 
 /** A `div` with margin between itself and its siblings. */
-const Row = ({ className, ...props }: RowProps) => (
+const Row = React.forwardRef<HTMLDivElement, RowProps>(({
+	className,
+	...props
+}, ref) => (
 	<div
 		className={`row${className ? ` ${className}` : ''}`}
 		{...props}
+		ref={ref}
 	/>
-);
+));
 
 export default Row;
