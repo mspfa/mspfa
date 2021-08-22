bd4cf7645ddbde6e0e7a2c5ebde018900379f49f
Grant Gryczan <grantgryczan@gmail.com>
2021-08-22 19:24:07 -0400

    Fix `Row` comment

--- components/Row/index.tsx
+++ components/Row/index.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export type RowProps = HTMLAttributes<HTMLDivElement>;
 
-/** A `div` with margin between itself and its siblings. Spans all columns when placed in a grid. */
+/** A `div` with vertical margin between itself and its siblings. Spans all columns when placed in a `LabeledGrid`. */
 const Row = React.forwardRef<HTMLDivElement, RowProps>(({
 	className,
 	...props
