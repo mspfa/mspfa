b431316b5cf0ed18efc3bf811198652270728357
Grant Gryczan <grantgryczan@gmail.com>
2021-08-27 02:26:01 -0400

    Internal comment state implementation

--- components/StoryViewer/Basement/BasementContent/StoryComments/index.tsx
+++ components/StoryViewer/Basement/BasementContent/StoryComments/index.tsx
@@ -7,17 +7,123 @@ import type { FormikHelpers } from 'formik';
 import { Formik, Form } from 'formik';
 import type { APIClient } from 'lib/client/api';
 import api from 'lib/client/api';
+import type { ClientComment } from 'lib/client/comments';
+import Dialog from 'lib/client/Dialog';
 import { useLeaveConfirmation } from 'lib/client/forms';
+import frameThrottler from 'lib/client/frameThrottler';
 import IDPrefix from 'lib/client/IDPrefix';
 import useFunction from 'lib/client/useFunction';
-import React, { useContext } from 'react';
+import { useUserCache } from 'lib/client/UserCache';
+import { promptSignIn, useUser } from 'lib/client/users';
+import { addViewportListener, removeViewportListener } from 'lib/client/viewportListener';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 
+type StoryCommentsAPI = APIClient<typeof import('pages/api/stories/[storyID]/comments').default>;
 type StoryPageCommentsAPI = APIClient<typeof import('pages/api/stories/[storyID]/pages/[pageID]/comments').default>;
+
+/** The maximum number of comments to request each time. */
+const COMMENTS_PER_REQUEST = 10;
 
 const StoryComments = React.memo(() => {
 	const { story } = useContext(StoryViewerContext)!;
 
 	const pageID = useContext(PageIDContext);
+
+	const { cacheUser } = useUserCache();
+
+	const user = useUser();
+
+	const [comments, setComments] = useState<ClientComment[]>([]);
+
+	const [notAllCommentsLoaded, setNotAllCommentsLoaded] = useState(true);
+	/** A ref to whether comments are currently being requested. */
+	const loadingCommentsRef = useRef(false);
+	const commentsElementRef = useRef<HTMLDivElement>(null);
+
+	const checkIfCommentsShouldBeFetched = useFunction(async () => {
+		if (loadingCommentsRef.current) {
+			return;
+		}
+
+		const commentsRect = commentsElementRef.current!.getBoundingClientRect();
+		const commentsStyle = window.getComputedStyle(commentsElementRef.current!);
+		const commentsPaddingBottom = +commentsStyle.paddingBottom.slice(0, -2);
+		const commentsContentBottom = commentsRect.bottom - commentsPaddingBottom;
+
+		// Check if the user has scrolled below the bottom of the comment area.
+		if (commentsContentBottom < document.documentElement.clientHeight) {
+			loadingCommentsRef.current = true;
+
+			// Fetch more comments.
+			const {
+				data: {
+					comments: newComments,
+					userCache: newUserCache
+				}
+			} = await (api as StoryCommentsAPI).get(`/stories/${story.id}/comments`, {
+				params: {
+					fromPageID: pageID,
+					limit: COMMENTS_PER_REQUEST,
+					...comments.length && {
+						before: comments[comments.length - 1].id
+					}
+				}
+			}).finally(() => {
+				loadingCommentsRef.current = false;
+			});
+
+			if (newComments.length < COMMENTS_PER_REQUEST) {
+				setNotAllCommentsLoaded(false);
+			}
+
+			if (newComments.length === 0) {
+				return;
+			}
+
+			newUserCache.forEach(cacheUser);
+
+			setComments(comments => [
+				...comments,
+				...newComments
+			]);
+		}
+	});
+
+	useEffect(() => {
+		if (notAllCommentsLoaded) {
+			const _viewportListener = addViewportListener(checkIfCommentsShouldBeFetched);
+			frameThrottler(_viewportListener).then(checkIfCommentsShouldBeFetched);
+
+			return () => {
+				removeViewportListener(_viewportListener);
+			};
+		}
+
+		// `comments` must be a dependency here so that updating it calls `checkIfCommentsShouldBeFetched` again without needing to change the viewport.
+	}, [checkIfCommentsShouldBeFetched, notAllCommentsLoaded, comments]);
+
+	const deleteComment = useFunction((commentsID: string) => {
+		setComments(comments => {
+			const commentsIndex = comments.findIndex(({ id }) => id === commentsID);
+
+			return [
+				...comments.slice(0, commentsIndex),
+				...comments.slice(commentsIndex + 1, comments.length)
+			];
+		});
+	});
+
+	const setComment = useFunction((commentsPost: ClientComment) => {
+		setComments(comments => {
+			const commentsIndex = comments.findIndex(({ id }) => id === commentsPost.id);
+
+			return [
+				...comments.slice(0, commentsIndex),
+				commentsPost,
+				...comments.slice(commentsIndex + 1, comments.length)
+			];
+		});
+	});
 
 	return (
 		<IDPrefix.Provider value="story-comments">
@@ -28,11 +134,29 @@ const StoryComments = React.memo(() => {
 						values: { content: string },
 						formikHelpers: FormikHelpers<{ content: string }>
 					) => {
+						if (!user) {
+							// If `user` is undefined, this component cannot posibly be `active`, which means the user is trying to add a favorite without being signed in.
+
+							if (await Dialog.confirm({
+								id: 'post-comment',
+								title: 'Comment',
+								content: 'Sign in to post a comment!',
+								actions: ['Sign In', 'Cancel']
+							})) {
+								promptSignIn();
+							}
+
+							return;
+						}
+
 						const { data: comment } = await (api as StoryPageCommentsAPI).post(`/stories/${story.id}/pages/${pageID}/comments`, {
 							content: values.content
 						});
 
-						console.log(comment); // TODO: Render new comment.
+						setComments(comments => [
+							comment,
+							...comments
+						]);
 
 						formikHelpers.setFieldValue('content', '');
 					})
@@ -66,8 +190,11 @@ const StoryComments = React.memo(() => {
 					);
 				}}
 			</Formik>
-			<Row className="story-comments">
-				comments here
+			<Row
+				className="story-comments"
+				ref={commentsElementRef}
+			>
+				{}
 			</Row>
 		</IDPrefix.Provider>
 	);
