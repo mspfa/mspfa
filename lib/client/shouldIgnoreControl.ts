76d34561bb6d1f8145d4929c6790acbc1e151060
Grant Gryczan <grantgryczan@gmail.com>
2021-09-09 18:24:20 -0400

    Fix `shouldIgnoreControl` for `contentEditable`

--- lib/client/shouldIgnoreControl.ts
+++ lib/client/shouldIgnoreControl.ts
@@ -1,7 +1,11 @@
 /** Returns a boolean for whether a keyboard control should be ignored. */
 const shouldIgnoreControl = () => (
-	// Check if the element currenly in focus has a `select` method i.e. if it is a text input.
-	(document.activeElement as any)?.select instanceof Function
+	!!document.activeElement && (
+		// Check if the element currenly in focus has a `select` method, which means it's a text input.
+		(document.activeElement as any).select instanceof Function
+		// Check if it's a `contentEditable` element.
+		|| (document.activeElement as HTMLElement).contentEditable === 'true'
+	)
 );
 
 export default shouldIgnoreControl;
