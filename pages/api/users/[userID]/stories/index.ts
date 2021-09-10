fd18a1a0141e9d3575801b1ef8dfd5d9e875a8ae
Grant Gryczan <grantgryczan@gmail.com>
2021-09-09 21:46:24 -0400

    Allow API to get one's own non-public stories

--- pages/api/users/[userID]/stories/index.ts
+++ pages/api/users/[userID]/stories/index.ts
@@ -2,7 +2,9 @@ import validate from './index.validate';
 import type { APIHandler } from 'lib/server/api';
 import type { PublicStory } from 'lib/client/stories';
 import { getUserByUnsafeID } from 'lib/server/users';
-import { getPublicStoriesByEditor } from 'lib/server/stories';
+import stories, { getPublicStoriesByEditor, getPublicStory } from 'lib/server/stories';
+import { authenticate } from 'lib/server/auth';
+import { Perm } from 'lib/client/perms';
 
 const Handler: APIHandler<{
 	query: {
@@ -14,9 +16,28 @@ const Handler: APIHandler<{
 }> = async (req, res) => {
 	await validate(req, res);
 
-	const user = await getUserByUnsafeID(req.query.userID, res);
+	const editor = await getUserByUnsafeID(req.query.userID, res);
 
-	res.send(await getPublicStoriesByEditor(user));
+	const { user } = await authenticate(req, res);
+
+	if (user && (
+		user._id.equals(editor._id)
+		|| user.perms & Perm.sudoRead
+	)) {
+		res.send(
+			await stories.find!({
+				$or: [{
+					owner: editor._id
+				}, {
+					editors: editor._id
+				}],
+				willDelete: { $exists: false }
+			}).map(getPublicStory).toArray()
+		);
+		return;
+	}
+
+	res.send(await getPublicStoriesByEditor(editor));
 };
 
 export default Handler;
