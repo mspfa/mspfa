df700ab3cbfa619d4da669b2aa8a0e769e1a8be2
Grant Gryczan <grantgryczan@gmail.com>
2021-09-19 13:25:38 -0400

    Update `escapeBBAttribute` for new BBCode parser

--- lib/client/escapeBBAttribute.ts
+++ lib/client/escapeBBAttribute.ts
@@ -1,14 +1,14 @@
 import escapeHTMLTags from 'lib/client/escapeHTMLTags';
 import replaceAll from 'lib/client/replaceAll';
 
-/** Escapes a user-inputted attribute value for use in BBCode. */
+/** Escapes and quotes a user-inputted attribute value for use in BBCode. */
 const escapeBBAttribute = (
-	/** The value of the attribute. */
+	/** The raw, unquoted value of the attribute. */
 	value: string,
-	/** Whether it's possible that the tag could be interpreted as having multiple attributes, and thus equal signs and extra quotation marks and apostrophes need to be escaped. */
+	/** Whether it's possible that the tag could be interpreted as having multiple attributes, and thus equal signs need to be escaped. */
 	possiblyMultipleAttributes?: boolean
 ) => {
-	// Escape angle brackets, since HTML is parsed before BBCode and could otherwise be parsed in the middle of a BB tag's attribute.
+	// Escape angle brackets, since HTML is parsed before BBCode and could otherwise be parsed in the middle of a BB tag's attribute, even if it's quoted.
 	value = escapeHTMLTags(value);
 
 	if (
@@ -22,11 +22,7 @@ const escapeBBAttribute = (
 		return `"${replaceAll(value, '"', '&quot;')}"`;
 	}
 
-	if (value[0] === '"' && (
-		// If there are possibly multiple attributes, the first character being `"` or `'` still needs to be escaped even if the last character isn't `"` or `'`, because otherwise, for example, `[spoiler show="' hide='"]` would be misinterpreted by the BBCode parser as having one attribute rather than two.
-		possiblyMultipleAttributes
-		|| value[value.length - 1] === '"'
-	)) {
+	if (value[0] === '"') {
 		if (value.includes('\'')) {
 			return `&quot;${value.slice(1)}`;
 		}
@@ -34,10 +30,7 @@ const escapeBBAttribute = (
 		return `'${value}'`;
 	}
 
-	if (value[0] === '\'' && (
-		possiblyMultipleAttributes
-		|| value[value.length - 1] === '\''
-	)) {
+	if (value[0] === '\'') {
 		if (value.includes('"')) {
 			return `&#39;${value.slice(1)}`;
 		}
