78836e5dce4136e76b736ca7cc3d6b090be1e77a
Grant Gryczan <grantgryczan@gmail.com>
2021-11-06 19:42:10 -0400

    Let cookies be stored outside `mspfa.com` on dev

--- lib/server/auth.ts
+++ lib/server/auth.ts
@@ -111,7 +111,12 @@ export const getAuthMethodInfo = async <AuthMethodType extends AuthMethod['type'
 
 const authCookieOptions: Cookies.SetOption = {
 	sameSite: 'strict',
-	domain: '.mspfa.com',
+	domain: (
+		process.env.NODE_ENV === 'development'
+			// Let the cookie be saved outside `mspfa.com` (e.g. on `localhost`) if running in dev mode.
+			? undefined
+			: '.mspfa.com'
+	),
 	maxAge: 1000 * 60 * 60 * 24 * 7
 };
 
