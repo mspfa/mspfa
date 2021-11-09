5c1256fabc2ada41c8cd6f61a52f7a7fab67c7ab
Grant Gryczan <grantgryczan@gmail.com>
2021-11-09 01:10:31 -0500

    Rename things in middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,15 +4,16 @@ import type { ErrorResponseBody } from 'lib/server/api';
 import type { DateNumber, integer } from 'lib/types';
 import type { NextRequest } from 'next/server';
 
+/** The number of milliseconds it takes for a client request time to be forgotten. */
 const TIME_PERIOD = 1000 * 60;
-/** The maximum number of requests that should be allowed from each client per `TIME_PERIOD`. */
+/** The maximum number of requests to allow from each client per `TIME_PERIOD`. */
 const MAX_REQUESTS_PER_TIME_PERIOD = 240;
 
 /** A mapping from each client's IP to a sorted array of the date numbers at which requests were received from that client. */
 const requestTimesByIP: Record<string, DateNumber[]> = {};
 
 /** Deletes all request times from `requestTimesByIP` which occurred longer than `TIME_PERIOD` ago. */
-const purgeOldRequestTimes = () => {
+const forgetOldRequestTimes = () => {
 	const timePeriodAgo = Date.now() - TIME_PERIOD;
 
 	for (const ip of Object.keys(requestTimesByIP)) {
@@ -28,7 +29,7 @@ const purgeOldRequestTimes = () => {
 	}
 };
 
-setInterval(purgeOldRequestTimes, 1000);
+setInterval(forgetOldRequestTimes, 1000);
 
 export const middleware = async (req: NextRequest) => {
 	let clientIP = req.headers.get('X-Forwarded-For');
@@ -53,7 +54,7 @@ export const middleware = async (req: NextRequest) => {
 	const clientRequestCount = clientRequestTimes.push(now);
 	if (clientRequestCount > MAX_REQUESTS_PER_TIME_PERIOD) {
 		/** The `DateNumber` after which the client will no longer be rate limited. */
-		// This is calculated by getting the time that the `MAX_REQUESTS_PER_TIME_PERIOD`th-last client request time will expire, because after that request time expires, the client would have one less than the `MAX_REQUESTS_PER_TIME_PERIOD`, which allows one more to be accepted without rate limiting.
+		// This is calculated by getting the time that the `MAX_REQUESTS_PER_TIME_PERIOD`th-last client request time will be forgotten, because after that request time is forgotten, the client would have one less than the `MAX_REQUESTS_PER_TIME_PERIOD`, which allows one more to be accepted without rate limiting.
 		const retryAfter = clientRequestTimes[clientRequestCount - MAX_REQUESTS_PER_TIME_PERIOD] + TIME_PERIOD;
 
 		const message = `You're sending data to MSPFA too quickly. Please wait ~${Math.ceil((retryAfter - now) / 1000)} seconds before retrying.`;
