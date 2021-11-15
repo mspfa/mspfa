05b2d33612c65d35e4e3fe7292321e4ec1e03e4f
Grant Gryczan <grantgryczan@gmail.com>
2021-11-15 18:32:50 -0500

    Comment on default behavior of `StoryIDContext`

--- lib/client/StoryIDContext.ts
+++ lib/client/StoryIDContext.ts
@@ -1,7 +1,11 @@
 import type { StoryID } from 'lib/server/stories';
 import React from 'react';
 
-/** A React context for the ID of the story which is the focus of the page. */
+/**
+ * A React context for the ID of the story which is the focus of the page.
+ *
+ * Undefined when there is no story which is the focus of the page.
+ */
 const StoryIDContext = React.createContext<StoryID | undefined>(undefined);
 
 export default StoryIDContext;
