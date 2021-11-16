2a2b8df209204922b1c079a69be80b600658ce7c
Grant Gryczan <grantgryczan@gmail.com>
2021-11-16 02:13:07 -0500

    Fix color button title redundancy

--- components/ColorTool/ColorButton/index.tsx
+++ components/ColorTool/ColorButton/index.tsx
@@ -17,7 +17,11 @@ const ColorButton = ({
 		id={`color-button-${color.id}`}
 		icon
 		className="color-button"
-		title={`${color.name} (${color.value})`}
+		title={
+			color.name === color.value
+				? color.name
+				: `${color.name} (${color.value})`
+		}
 		style={{
 			'--button-color': color.value
 		} as CSSProperties}
