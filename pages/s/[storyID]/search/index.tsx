3124c01701e5d61245b410ce6a1b4469d1d97f24
Grant Gryczan <grantgryczan@gmail.com>
2021-11-23 17:52:47 -0500

    Simplify search page rendering

--- pages/s/[storyID]/search/index.tsx
+++ pages/s/[storyID]/search/index.tsx
@@ -21,11 +21,11 @@ import StoryPageLink from 'components/StoryPageLink';
 import PreviewModeContext from 'lib/client/PreviewModeContext';
 import StoryIDContext from 'lib/client/StoryIDContext';
 
-type StorySearchResults = Array<Pick<ClientStoryPage, 'id' | 'published' | 'title' | 'content'>>;
+type StorySearchResult = Pick<ClientStoryPage, 'id' | 'published' | 'title' | 'content'>;
 
 type ServerSideProps = {
 	story: PublicStory,
-	results: StorySearchResults
+	results: StorySearchResult[]
 } | {
 	statusCode: integer
 };
@@ -43,87 +43,57 @@ const Component = withErrorPage<ServerSideProps>(({ story, results }) => {
 
 	let matches = 0;
 
-	const resultNodes = results.map(result => {
-		const getNodes = (string: string) => {
-			/**
-			 * The array of nodes for the marked string.
-			 *
-			 * In order to ensure unique keys, each node's key must be the index of the node's content in the original string, and no node can be empty.
-			 */
-			const nodes: ReactNode[] = [];
+	const markResults = (string: string) => {
+		/**
+		 * The array of nodes for the marked string.
+		 *
+		 * In order to ensure unique keys, each node's key must be the index of the node's content in the original string, and no node can be empty.
+		 */
+		const nodes: ReactNode[] = [];
 
-			// TODO: Accommodate the fact that `toLowerCase` can change a string's length, causing indexes in `stringToSearch` not to match up with indexes in `string`.
-			const stringToSearch = string.toLowerCase();
-			let index = stringToSearch.indexOf(searchQuery);
+		// TODO: Accommodate the fact that `toLowerCase` can change a string's length, causing indexes in `stringToSearch` not to match up with indexes in `string`.
+		const stringToSearch = string.toLowerCase();
+		let index = stringToSearch.indexOf(searchQuery);
 
-			// Ensure this slice won't be empty due to the start of the string equaling the start of the next match.
-			if (index !== 0) {
+		// Ensure this slice won't be empty due to the start of the string equaling the start of the next match.
+		if (index !== 0) {
+			nodes.push(
+				<Fragment key={0}>
+					{(index === -1
+						? string
+						: string.slice(0, index)
+					)}
+				</Fragment>
+			);
+		}
+
+		while (index !== -1) {
+			const endIndex = index + searchQuery.length;
+
+			nodes.push(
+				<mark key={index}>
+					{string.slice(index, endIndex)}
+				</mark>
+			);
+			matches++;
+
+			index = stringToSearch.indexOf(searchQuery, endIndex);
+
+			// Ensure this slice won't be empty due to the end of this match equaling the start of the next match.
+			if (endIndex !== index) {
 				nodes.push(
-					<Fragment key={0}>
+					<Fragment key={endIndex}>
 						{(index === -1
-							? string
-							: string.slice(0, index)
+							? string.slice(endIndex)
+							: string.slice(endIndex, index)
 						)}
 					</Fragment>
 				);
 			}
+		}
 
-			while (index !== -1) {
-				const endIndex = index + searchQuery.length;
-
-				nodes.push(
-					<mark key={index}>
-						{string.slice(index, endIndex)}
-					</mark>
-				);
-				matches++;
-
-				index = stringToSearch.indexOf(searchQuery, endIndex);
-
-				// Ensure this slice won't be empty due to the end of this match equaling the start of the next match.
-				if (endIndex !== index) {
-					nodes.push(
-						<Fragment key={endIndex}>
-							{(index === -1
-								? string.slice(endIndex)
-								: string.slice(endIndex, index)
-							)}
-						</Fragment>
-					);
-				}
-			}
-
-			return nodes;
-		};
-
-		return (
-			<div
-				key={result.id}
-				className="story-search-result"
-			>
-				<div className="story-search-result-heading">
-					<div className="story-search-result-timestamp-container">
-						{result.published === undefined ? (
-							'Draft'
-						) : (
-							<Timestamp short relative>
-								{result.published}
-							</Timestamp>
-						)}
-					</div>
-					<StoryPageLink
-						className="story-search-result-title"
-						pageID={result.id}
-					>
-						{getNodes(result.title)}
-					</StoryPageLink>
-				</div>
-				<div className="story-search-result-content">
-					{getNodes(result.content)}
-				</div>
-			</div>
-		);
-	});
+		return nodes;
+	};
 
 	const pageComponent = (
 		<Page withFlashyTitle heading="Adventure Search">
@@ -163,7 +133,33 @@ const Component = withErrorPage<ServerSideProps>(({ story, results }) => {
 					{`${matches} result${matches === 1 ? '' : 's'} in ${results.length} page${results.length === 1 ? '' : 's'}`}
 				</div>
 				<div id="story-search-results">
-					{resultNodes}
+					{results.map(result => (
+						<div
+							key={result.id}
+							className="story-search-result"
+						>
+							<div className="story-search-result-heading">
+								<div className="story-search-result-timestamp-container">
+									{result.published === undefined ? (
+										'Draft'
+									) : (
+										<Timestamp short relative>
+											{result.published}
+										</Timestamp>
+									)}
+								</div>
+								<StoryPageLink
+									className="story-search-result-title"
+									pageID={result.id}
+								>
+									{markResults(result.title)}
+								</StoryPageLink>
+							</div>
+							<div className="story-search-result-content">
+								{markResults(result.content)}
+							</div>
+						</div>
+					))}
 				</div>
 			</Section>
 		</Page>
@@ -208,7 +204,7 @@ export const getServerSideProps = withStatusCode<ServerSideProps>(async ({ req, 
 			: ''
 	);
 
-	const results: StorySearchResults = [];
+	const results: StorySearchResult[] = [];
 
 	if (searchQuery) {
 		const lastPageID = (
