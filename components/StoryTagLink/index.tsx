0e469c7f3f1bc31a84fcd9e875d2c84eabd5ced9
Grant Gryczan <grantgryczan@gmail.com>
2021-11-24 16:08:31 -0500

    Show tag info on hover over `StoryTagLink`

--- components/StoryTagLink/index.tsx
+++ components/StoryTagLink/index.tsx
@@ -1,5 +1,6 @@
 import './styles.module.scss';
 import Link from 'components/Link';
+import storyTags from 'lib/client/storyTags';
 
 export type StoryTagLinkProps = {
 	children: string
@@ -7,7 +8,10 @@ export type StoryTagLinkProps = {
 
 /** A link for a story's tag. Should always be directly inside an `InconspicuousDiv`. */
 const StoryTagLink = ({ children }: StoryTagLinkProps) => (
-	<Link className="story-tag-link">
+	<Link
+		className="story-tag-link"
+		title={storyTags[children] || '(Unknown Tag)'}
+	>
 		{`#${children}`}
 	</Link>
 );
