22b473bfd31f679603ceda868a17c873bfabe50b
Grant Gryczan <grantgryczan@gmail.com>
2021-11-27 20:50:43 -0500

    Add reverse option for story sorting

--- pages/browse/stories/index.tsx
+++ pages/browse/stories/index.tsx
@@ -12,8 +12,53 @@ import StoryListing from 'components/StoryListing';
 import TagField from 'components/TagField';
 import type StoryStatus from 'lib/client/StoryStatus';
 import { storyStatusNames } from 'lib/client/StoryStatus';
-import type { ReactNode } from 'react';
+import type { ChangeEvent, ReactNode } from 'react';
+import { useState } from 'react';
 import type { StorySortMode } from 'pages/api/stories';
+import useFunction from 'lib/client/reactHooks/useFunction';
+
+/** A record which maps every `StoryStatus` to `true`. */
+const allStatusesTrue: Record<string, true> = {} as any;
+
+const statusFieldContainers: ReactNode[] = [];
+
+for (const status of Object.keys(storyStatusNames)) {
+	allStatusesTrue[status] = true;
+
+	statusFieldContainers.push(
+		<span
+			key={status}
+			className="browse-stories-checkbox-field-container"
+		>
+			<Field
+				type="checkbox"
+				id={`field-status-${status}`}
+				name={`status.${status}`}
+				className="spaced"
+			/>
+			<label
+				className="spaced"
+				htmlFor={`field-status-${status}`}
+			>
+				{storyStatusNames[status as unknown as StoryStatus]}
+			</label>
+		</span>
+	);
+}
+
+/** A mapping from each `StorySortMode` to the `StorySortMode` which is in reverse from it. */
+const reversedSorts: Record<StorySortMode, StorySortMode> = {
+	titleIndex: 'titleIndex',
+	leastFavs: 'mostFavs',
+	fewestPages: 'mostPages',
+	oldestCreated: 'newestCreated',
+	newestUpdated: 'oldestUpdated',
+	mostFavs: 'leastFavs',
+	mostPages: 'fewestPages',
+	newestCreated: 'oldestCreated',
+	oldestUpdated: 'newestUpdated',
+	random: 'random'
+};
 
 type ServerSideProps = {
 	stories?: never,
@@ -26,34 +71,7 @@ type ServerSideProps = {
 const Component = ({ stories, resultCount }: ServerSideProps) => {
 	const router = useRouter();
 
-	/** A record which maps every `StoryStatus` to `true`. */
-	const allStatusesTrue: Record<string, true> = {} as any;
-
-	const statusFieldContainers: ReactNode[] = [];
-
-	for (const status of Object.keys(storyStatusNames)) {
-		allStatusesTrue[status] = true;
-
-		statusFieldContainers.push(
-			<span
-				key={status}
-				className="browse-stories-field-container-status"
-			>
-				<Field
-					type="checkbox"
-					id={`field-status-${status}`}
-					name={`status.${status}`}
-					className="spaced"
-				/>
-				<label
-					className="spaced"
-					htmlFor={`field-status-${status}`}
-				>
-					{storyStatusNames[status as unknown as StoryStatus]}
-				</label>
-			</span>
-		);
-	}
+	const [sortReverse, setSortReverse] = useState(false);
 
 	return (
 		<BrowsePage
@@ -68,49 +86,92 @@ const Component = ({ stories, resultCount }: ServerSideProps) => {
 			resultCount={resultCount}
 			results={stories}
 		>
-			<Row>
-				<Label className="spaced" htmlFor="field-title">
-					Title
-				</Label>
-				<Field
-					id="field-title"
-					name="title"
-					className="spaced"
-					maxLength={50}
-					autoFocus
-				/>
-			</Row>
-			<Row>
-				<Label className="spaced">
-					Status
-				</Label>
-				{statusFieldContainers}
-			</Row>
-			<Row>
-				<TagField allowExcludedTags />
-			</Row>
-			<Row>
-				<Label className="spaced" htmlFor="field-sort">
-					Sort By
-				</Label>
-				<Field
-					as="select"
-					id="field-sort"
-					name="sort"
-					className="spaced"
-				>
-					<option value="titleIndex">Title Relevance</option>
-					<option value="mostFavs">Most Favorites</option>
-					<option value="leastFavs">Least Favorites</option>
-					<option value="mostPages">Most Pages</option>
-					<option value="fewestPages">Fewest Pages</option>
-					<option value="newestCreated">Most Recently Created</option>
-					<option value="oldestCreated">Least Recently Created</option>
-					<option value="oldestUpdated">Most Recently Updated</option>
-					<option value="newestUpdated">Least Recently Updated</option>
-					<option value="random">Random</option>
-				</Field>
-			</Row>
+			{function BrowsePageContent({ values, setFieldValue }) {
+				/** The reverse of the `sort` value. */
+				const reversedSort = reversedSorts[values.sort];
+				/** Whether the `sort` value should remain the same when `sortReverse`. */
+				const symmetricalSort = values.sort === reversedSort;
+
+				return (
+					<>
+						<Row>
+							<Label className="spaced" htmlFor="field-title">
+								Title
+							</Label>
+							<Field
+								id="field-title"
+								name="title"
+								className="spaced"
+								maxLength={50}
+								autoFocus
+							/>
+						</Row>
+						<Row>
+							<Label className="spaced">
+								Status
+							</Label>
+							{statusFieldContainers}
+						</Row>
+						<Row>
+							<TagField allowExcludedTags />
+						</Row>
+						<Row>
+							<Label className="spaced" htmlFor="field-sort">
+								Sort By
+							</Label>
+							<Field
+								as="select"
+								id="field-sort"
+								name="sort"
+								className="spaced"
+							>
+								<option value="titleIndex">Title Relevance</option>
+								{sortReverse ? (
+									<>
+										<option value="leastFavs">Least Favorites</option>
+										<option value="fewestPages">Fewest Pages</option>
+										<option value="oldestCreated">Least Recently Created</option>
+										<option value="newestUpdated">Least Recently Updated</option>
+									</>
+								) : (
+									<>
+										<option value="mostFavs">Most Favorites</option>
+										<option value="mostPages">Most Pages</option>
+										<option value="newestCreated">Most Recently Created</option>
+										<option value="oldestUpdated">Most Recently Updated</option>
+									</>
+								)}
+								<option value="random">Random</option>
+							</Field>
+							<span
+								className={`browse-stories-checkbox-field-container${symmetricalSort ? ' translucent' : ''}`}
+							>
+								<input
+									type="checkbox"
+									id="field-sort-reverse"
+									className="spaced"
+									checked={sortReverse}
+									onChange={
+										useFunction((event: ChangeEvent<HTMLInputElement>) => {
+											setSortReverse(event.target.checked);
+
+											if (!symmetricalSort) {
+												setFieldValue('sort', reversedSort);
+											}
+										})
+									}
+								/>
+								<label
+									htmlFor="field-sort-reverse"
+									className="spaced"
+								>
+									Reverse
+								</label>
+							</span>
+						</Row>
+					</>
+				);
+			}}
 		</BrowsePage>
 	);
 };
