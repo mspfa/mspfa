3de4d2a91d2522da61bed3cd246efbe2554db2bf
Grant Gryczan <grantgryczan@gmail.com>
2021-12-09 21:33:11 -0500

    Set `href` on `StoryTagLink`

--- components/StoryTagLink/index.tsx
+++ components/StoryTagLink/index.tsx
@@ -1,19 +1,20 @@
 import './styles.module.scss';
 import Link from 'components/Link';
+import type { TagString } from 'lib/client/storyTags';
 import storyTags from 'lib/client/storyTags';
 
 export type StoryTagLinkProps = {
-	children: string
+	children: TagString
 };
 
 /** A link for a story's tag. Should always be directly inside an `InconspicuousDiv`. */
-const StoryTagLink = ({ children }: StoryTagLinkProps) => (
+const StoryTagLink = ({ children: tagString }: StoryTagLinkProps) => (
 	<Link
 		className="story-tag-link"
-		href="/TODO"
-		title={storyTags[children] || '(Custom Tag)'}
+		href={`/browse/stories?tags=${tagString}`}
+		title={storyTags[tagString] || '(Custom Tag)'}
 	>
-		{`#${children}`}
+		{`#${tagString}`}
 	</Link>
 );
 
