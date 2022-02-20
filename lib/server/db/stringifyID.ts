29a4d77c5e54f2e545c5055cbc691c287cfde307
Grant Gryczan <grantgryczan@gmail.com>
2022-02-20 03:04:05 -0500

    Simplify `stringifyID`

--- lib/server/db/stringifyID.ts
+++ lib/server/db/stringifyID.ts
@@ -1,6 +1,6 @@
 import type { ObjectId } from 'mongodb';
 
 /** Converts an `ObjectId` into a Base64 URL string. */
-const stringifyID = (id: ObjectId) => id.id.toString('base64url');
+const stringifyID = (id: ObjectId) => id.toString('base64url');
 
 export default stringifyID;
