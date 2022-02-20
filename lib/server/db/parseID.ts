44c33edbeed38ae5a0824e452cb69303163484b3
Grant Gryczan <grantgryczan@gmail.com>
2022-02-19 22:49:09 -0500

    Fix Base64 URL encoding

--- lib/server/db/parseID.ts
+++ lib/server/db/parseID.ts
@@ -1,19 +1,14 @@
-import replaceAll from 'lib/client/replaceAll';
 import { ObjectId } from 'mongodb';
 
-/** Parses the inputted Base64-ish string as an `ObjectId`, or returns `undefined` if the input is invalid. */
+/** Parses the inputted Base64 URL string as an `ObjectId`, or returns `undefined` if the input is invalid. */
 const parseID = (string: string | undefined) => {
 	if (string === undefined) {
 		return;
 	}
 
 	try {
-		// Undo the changes to the Base64 alphabet done by `stringifyID`.
-		string = replaceAll(string, '-', '/');
-		string = replaceAll(string, '_', '+');
-
 		return new ObjectId(
-			Buffer.from(string, 'base64')
+			Buffer.from(string, 'base64url')
 		);
 	} catch {}
 };
--- lib/server/db/stringifyID.ts
+++ lib/server/db/stringifyID.ts
@@ -1,17 +1,6 @@
-import replaceAll from 'lib/client/replaceAll';
 import type { ObjectId } from 'mongodb';
 
-/** Converts an `ObjectId` into a Base64-ish string. */
-const stringifyID = (id: ObjectId) => {
-	let string = id.id.toString('base64');
-
-	// Alter the Base64 alphabet to be nicer for URLs.
-	string = replaceAll(string, '/', '-');
-	string = replaceAll(string, '+', '_');
-
-	// Note: It is impossible for the string to have equal signs, since the `id.id` buffer is always exactly 12 bytes.
-
-	return string;
-};
+/** Converts an `ObjectId` into a Base64 URL string. */
+const stringifyID = (id: ObjectId) => id.id.toString('base64url');
 
 export default stringifyID;
