67cc444173d4f508268ae2945360e2d9695f09ce
Grant Gryczan <grantgryczan@gmail.com>
2022-12-28 23:35:00 -0500

    Remove unnecessary `Captcha` effect

--- components/Captcha/index.tsx
+++ components/Captcha/index.tsx
@@ -1,7 +1,6 @@
 import './styles.module.scss';
 import { signInValues, initialSignInValues } from 'components/SignIn';
 import HCaptcha from '@hcaptcha/react-hcaptcha';
-import { useEffect } from 'react';
 
 const onCaptchaVerify = (token: string) => {
 	signInValues.captchaToken = token;
@@ -11,17 +10,13 @@ const resetCaptchaToken = () => {
 	signInValues.captchaToken = initialSignInValues.captchaToken;
 };
 
-const Captcha = () => {
-	useEffect(resetCaptchaToken, []);
-
-	return (
-		<HCaptcha
-			id="captcha"
-			sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY!}
-			onVerify={onCaptchaVerify}
-			onExpire={resetCaptchaToken}
-		/>
-	);
-};
+const Captcha = () => (
+	<HCaptcha
+		id="captcha"
+		sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY!}
+		onVerify={onCaptchaVerify}
+		onExpire={resetCaptchaToken}
+	/>
+);
 
 export default Captcha;
