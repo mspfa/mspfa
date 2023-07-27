37f524247b92870ff9418e77f1ff10502dca1994
Grant Gryczan <grantgryczan@gmail.com>
2023-07-27 03:49:12 -0400

    Fix `SameSite` attribute

--- lib/server/auth/authCookieOptions.ts
+++ lib/server/auth/authCookieOptions.ts
@@ -1,8 +1,8 @@
 import type { SetOption } from 'cookies';
 
 const authCookieOptions: SetOption = {
-	// Protect against CSRF attacks.
-	sameSite: 'strict',
+	// Protect against CSRF attacks. Don't use `strict` because that'd make external links open an unauthenticated page.
+	sameSite: 'lax',
 	domain: (
 		process.env.NODE_ENV === 'development'
 			// Let the cookie be saved outside `mspfa.com` (e.g. on `localhost`) if running in dev mode.
